55d5280233934a28cba7cc5b5ec3dc3d428f7bd3
Samuel Abels <knipknap@gmail.com>
2010-01-15 01:58:25 +0100

    Get rid of the Server directory, which was just a crappy prototype.

--- src/SpiffWorkflow/Server/__init__.py
+++
@@ -1,9 +0,0 @@
-from DB           import DB
-from Driver       import Driver
-from WorkflowInfo import WorkflowInfo
-from JobInfo      import JobInfo
-from TaskInfo     import TaskInfo
-
-import inspect
-__all__ = [name for name, obj in locals().items()
-           if not (name.startswith('_') or inspect.ismodule(obj))]
