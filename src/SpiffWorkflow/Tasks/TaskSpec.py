533ac25e3ca07d4b0762579581540b1d95336ee0
Samuel Abels <knipknap@gmail.com>
2010-11-18 13:40:32 +0100

    use the packaged SpiffSignal instead of the globally installed one.

--- src/SpiffWorkflow/Tasks/TaskSpec.py
+++ src/SpiffWorkflow/Tasks/TaskSpec.py
@@ -13,10 +13,10 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-from SpiffSignal             import Trackable
-from SpiffWorkflow.Task      import Task
-from SpiffWorkflow.Exception import WorkflowException
-from SpiffWorkflow.Operators import valueof
+from SpiffWorkflow.external.SpiffSignal import Trackable
+from SpiffWorkflow.Task                 import Task
+from SpiffWorkflow.Exception            import WorkflowException
+from SpiffWorkflow.Operators            import valueof
 
 class TaskSpec(Trackable):
     """
