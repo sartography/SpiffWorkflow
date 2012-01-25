bdf64b9b89d0754a7c6c32e246e2f1025fcfcba2
Filipe Correia <fcorreia@gmail.com>
2012-01-25 12:12:05 +0000

    Refactoring WorkflowTest.py in preparation to adding a new test

--- tests/SpiffWorkflow/WorkflowTest.py
+++ tests/SpiffWorkflow/WorkflowTest.py
@@ -64,136 +64,96 @@ class WorkflowTest(unittest.TestCase):
     change will break both tests!
     '''
     def setUp(self):
-        self.wf            = Workflow()
-        self.expected_path = [( 1, 'Start'),
-                              ( 2, 'task_a1'),
-                              ( 3, 'task_a2'),
-                              ( 2, 'task_b1'),
-                              ( 3, 'task_b2'),
-                              ( 4, 'synch_1'),
-                              ( 5, 'excl_choice_1'),
-                              ( 6, 'task_c1'),
-                              ( 7, 'excl_choice_2'),
-                              ( 8, 'task_d3'),
-                              ( 9, 'multi_choice_1'),
-                              (10, 'task_e1'),
-                              (10, 'task_e3'),
-                              (11, 'struct_synch_merge_1'),
-                              (12, 'task_f1'),
-                              (13, 'struct_discriminator_1'),
-                              (14, 'excl_choice_3'),
-                              (15, 'excl_choice_1'),
-                              (16, 'task_c1'),
-                              (17, 'excl_choice_2'),
-                              (18, 'task_d3'),
-                              (19, 'multi_choice_1'),
-                              (20, 'task_e1'),
-                              (20, 'task_e3'),
-                              (21, 'struct_synch_merge_1'),
-                              (22, 'task_f1'),
-                              (23, 'struct_discriminator_1'),
-                              (24, 'excl_choice_3'),
-                              (25, 'multi_instance_1'),
-                              (26, 'task_g1'),
-                              (26, 'task_g2'),
-                              (26, 'task_g1'),
-                              (26, 'task_g2'),
-                              (26, 'task_g1'),
-                              (26, 'task_g2'),
-                              (27, 'struct_synch_merge_2'),
-                              (28, 'last'),
-                              (29, 'End'),
-                              (22, 'task_f2'),
-                              (22, 'task_f3'),
-                              (12, 'task_f2'),
-                              (12, 'task_f3')]
+        self.wf = self._getWorkflow()
+        self.expected_path = self._getExpectedPath()
 
+    def _getWorkflow(self):
+        wf = Workflow()
 
-    def testWorkflow(self):
         # Build one branch.
-        a1 = Simple(self.wf, 'task_a1')
-        self.wf.start.connect(a1)
+        a1 = Simple(wf, 'task_a1')
+        wf.start.connect(a1)
 
-        a2 = Simple(self.wf, 'task_a2')
+        a2 = Simple(wf, 'task_a2')
         a1.connect(a2)
 
         # Build another branch.
-        b1 = Simple(self.wf, 'task_b1')
-        self.wf.start.connect(b1)
+        b1 = Simple(wf, 'task_b1')
+        wf.start.connect(b1)
 
-        b2 = Simple(self.wf, 'task_b2')
+        b2 = Simple(wf, 'task_b2')
         b1.connect(b2)
 
         # Merge both branches (synchronized).
-        synch_1 = Join(self.wf, 'synch_1')
+        synch_1 = Join(wf, 'synch_1')
         a2.connect(synch_1)
         b2.connect(synch_1)
 
         # If-condition that does not match.
-        excl_choice_1 = ExclusiveChoice(self.wf, 'excl_choice_1')
+        excl_choice_1 = ExclusiveChoice(wf, 'excl_choice_1')
         synch_1.connect(excl_choice_1)
 
-        c1 = Simple(self.wf, 'task_c1')
+        c1 = Simple(wf, 'task_c1')
         excl_choice_1.connect(c1)
 
-        c2 = Simple(self.wf, 'task_c2')
+        c2 = Simple(wf, 'task_c2')
         cond = Equal(Attrib('test_attribute1'), Attrib('test_attribute2'))
         excl_choice_1.connect_if(cond, c2)
 
-        c3 = Simple(self.wf, 'task_c3')
+        c3 = Simple(wf, 'task_c3')
         excl_choice_1.connect_if(cond, c3)
 
         # If-condition that matches.
-        excl_choice_2 = ExclusiveChoice(self.wf, 'excl_choice_2')
+        excl_choice_2 = ExclusiveChoice(wf, 'excl_choice_2')
         c1.connect(excl_choice_2)
         c2.connect(excl_choice_2)
         c3.connect(excl_choice_2)
 
-        d1 = Simple(self.wf, 'task_d1')
+        d1 = Simple(wf, 'task_d1')
         excl_choice_2.connect(d1)
 
-        d2 = Simple(self.wf, 'task_d2')
+        d2 = Simple(wf, 'task_d2')
         excl_choice_2.connect_if(cond, d2)
 
-        d3 = Simple(self.wf, 'task_d3')
+        d3 = Simple(wf, 'task_d3')
         cond = Equal(Attrib('test_attribute1'), Attrib('test_attribute1'))
         excl_choice_2.connect_if(cond, d3)
 
         # If-condition that does not match.
-        multichoice = MultiChoice(self.wf, 'multi_choice_1')
+        multichoice = MultiChoice(wf, 'multi_choice_1')
         d1.connect(multichoice)
         d2.connect(multichoice)
         d3.connect(multichoice)
 
-        e1 = Simple(self.wf, 'task_e1')
+        e1 = Simple(wf, 'task_e1')
         multichoice.connect_if(cond, e1)
 
-        e2 = Simple(self.wf, 'task_e2')
+        e2 = Simple(wf, 'task_e2')
         cond = Equal(Attrib('test_attribute1'), Attrib('test_attribute2'))
         multichoice.connect_if(cond, e2)
 
-        e3 = Simple(self.wf, 'task_e3')
+        e3 = Simple(wf, 'task_e3')
         cond = Equal(Attrib('test_attribute2'), Attrib('test_attribute2'))
         multichoice.connect_if(cond, e3)
 
         # StructuredSynchronizingMerge
-        syncmerge = Join(self.wf, 'struct_synch_merge_1', 'multi_choice_1')
+        syncmerge = Join(wf, 'struct_synch_merge_1', 'multi_choice_1')
         e1.connect(syncmerge)
         e2.connect(syncmerge)
         e3.connect(syncmerge)
 
         # Implicit parallel split.
-        f1 = Simple(self.wf, 'task_f1')
+        f1 = Simple(wf, 'task_f1')
         syncmerge.connect(f1)
 
-        f2 = Simple(self.wf, 'task_f2')
+        f2 = Simple(wf, 'task_f2')
         syncmerge.connect(f2)
 
-        f3 = Simple(self.wf, 'task_f3')
+        f3 = Simple(wf, 'task_f3')
         syncmerge.connect(f3)
 
         # Discriminator
-        discrim_1 = Join(self.wf,
+        discrim_1 = Join(wf,
                          'struct_discriminator_1',
                          'struct_synch_merge_1',
                          threshold = 1)
@@ -202,38 +162,82 @@ class WorkflowTest(unittest.TestCase):
         f3.connect(discrim_1)
 
         # Loop back to the first exclusive choice.
-        excl_choice_3 = ExclusiveChoice(self.wf, 'excl_choice_3')
+        excl_choice_3 = ExclusiveChoice(wf, 'excl_choice_3')
         discrim_1.connect(excl_choice_3)
         cond = NotEqual(Attrib('excl_choice_3_reached'), Attrib('two'))
         excl_choice_3.connect_if(cond, excl_choice_1)
 
         # Split into 3 branches, and implicitly split twice in addition.
-        multi_instance_1 = MultiInstance(self.wf, 'multi_instance_1', times = 3)
+        multi_instance_1 = MultiInstance(wf, 'multi_instance_1', times = 3)
         excl_choice_3.connect(multi_instance_1)
 
         # Parallel tasks.
-        g1 = Simple(self.wf, 'task_g1')
-        g2 = Simple(self.wf, 'task_g2')
+        g1 = Simple(wf, 'task_g1')
+        g2 = Simple(wf, 'task_g2')
         multi_instance_1.connect(g1)
         multi_instance_1.connect(g2)
 
         # StructuredSynchronizingMerge
-        syncmerge2 = Join(self.wf, 'struct_synch_merge_2', 'multi_instance_1')
+        syncmerge2 = Join(wf, 'struct_synch_merge_2', 'multi_instance_1')
         g1.connect(syncmerge2)
         g2.connect(syncmerge2)
 
         # Add a final task.
-        last = Simple(self.wf, 'last')
+        last = Simple(wf, 'last')
         syncmerge2.connect(last)
 
         # Add another final task :-).
-        end = Simple(self.wf, 'End')
+        end = Simple(wf, 'End')
         last.connect(end)
 
-        self.runWorkflow(self.wf)
+        return wf
 
+    def _getExpectedPath(self):
+        return [( 1, 'Start'),
+          ( 2, 'task_a1'),
+          ( 3, 'task_a2'),
+          ( 2, 'task_b1'),
+          ( 3, 'task_b2'),
+          ( 4, 'synch_1'),
+          ( 5, 'excl_choice_1'),
+          ( 6, 'task_c1'),
+          ( 7, 'excl_choice_2'),
+          ( 8, 'task_d3'),
+          ( 9, 'multi_choice_1'),
+          (10, 'task_e1'),
+          (10, 'task_e3'),
+          (11, 'struct_synch_merge_1'),
+          (12, 'task_f1'),
+          (13, 'struct_discriminator_1'),
+          (14, 'excl_choice_3'),
+          (15, 'excl_choice_1'),
+          (16, 'task_c1'),
+          (17, 'excl_choice_2'),
+          (18, 'task_d3'),
+          (19, 'multi_choice_1'),
+          (20, 'task_e1'),
+          (20, 'task_e3'),
+          (21, 'struct_synch_merge_1'),
+          (22, 'task_f1'),
+          (23, 'struct_discriminator_1'),
+          (24, 'excl_choice_3'),
+          (25, 'multi_instance_1'),
+          (26, 'task_g1'),
+          (26, 'task_g2'),
+          (26, 'task_g1'),
+          (26, 'task_g2'),
+          (26, 'task_g1'),
+          (26, 'task_g2'),
+          (27, 'struct_synch_merge_2'),
+          (28, 'last'),
+          (29, 'End'),
+          (22, 'task_f2'),
+          (22, 'task_f3'),
+          (12, 'task_f2'),
+          (12, 'task_f3')]
 
-    def runWorkflow(self, wf):
+    def testAutoCompleteWorkflow(self):
+        wf = self.wf
         taken_path = {'reached':   [],
                       'completed': []}
         for name, task in wf.tasks.iteritems():
