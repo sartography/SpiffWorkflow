3e5be6af0e8c75e96b9dfe3989eadabec5938eb8
Samuel Abels <knipknap@gmail.com>
2012-03-02 16:08:13 +0100

    OpenWfeReaderTest no longer depends on WorkflowTest.

--- tests/SpiffWorkflow/storage/OpenWfeXmlReaderTest.py
+++ tests/SpiffWorkflow/storage/OpenWfeXmlReaderTest.py
@@ -3,16 +3,14 @@ dirname = os.path.dirname(__file__)
 data_dir = os.path.join(dirname, '..', 'data')
 sys.path.insert(0, os.path.join(dirname, '..', '..', '..', 'src'))
 
-from WorkflowTest import on_reached_cb, on_complete_cb, assert_same_path
+from PatternTest import track_workflow
 from SpiffWorkflow import Workflow
 from SpiffWorkflow.storage import OpenWfeXmlReader
 from xml.parsers.expat import ExpatError
 
 class OpenWfeXmlReaderTest(unittest.TestCase):
     def setUp(self):
-        self.reader     = OpenWfeXmlReader()
-        self.taken_path = []
-
+        self.reader = OpenWfeXmlReader()
 
     def on_reached_cb(self, workflow, instance):
         on_reached_cb(workflow, instance, [])
@@ -20,13 +18,11 @@ class OpenWfeXmlReaderTest(unittest.TestCase):
         instance.set_attribute(test_attribute2 = 'true')
         return True
 
-
     def testParseString(self):
         self.assertRaises(ExpatError,
                           self.reader.parse_string,
                           '')
         self.reader.parse_string('<xml></xml>')
-
 
     def testParseFile(self):
         # File not found.
@@ -45,41 +41,25 @@ class OpenWfeXmlReaderTest(unittest.TestCase):
         # Read a complete workflow.
         self.reader.parse_file(os.path.join(data_dir, 'openwfe/workflow1.xml'))
 
-
     def testRunWorkflow(self):
-        filename = os.path.join(data_dir, 'openwfe/workflow1.xml')
-        wf_specs = self.reader.parse_file(filename)
-        wf_spec = wf_specs[0]
-
-        for name in wf_spec.task_specs:
-            wf_spec.task_specs[name].reached_event.connect(self.on_reached_cb)
-            wf_spec.task_specs[name].completed_event.connect(on_complete_cb, self.taken_path)
-
-        workflow = Workflow(wf_spec)
+        xml_file      = os.path.join(data_dir, 'openwfe/workflow1.xml')
+        path_file     = os.path.splitext(xml_file)[0] + '.path'
+        expected_path = open(path_file).read().strip().split('\n')
+        wf_specs      = self.reader.parse_file(xml_file)
+        wf_spec       = wf_specs[0]
+        taken_path    = track_workflow(wf_spec)
+        workflow      = Workflow(wf_spec)
         try:
             workflow.complete_all()
         except:
             workflow.dump()
             raise
 
-        path = [( 1, 'Start'),
-                ( 2, 'concurrence_1'),
-                ( 3, 'task_a1'),
-                ( 4, 'task_a2'),
-                ( 5, 'if_condition_1'),
-                ( 6, 'task_a3'),
-                ( 7, 'if_condition_1_end'),
-                ( 8, 'if_condition_2'),
-                ( 9, 'task_a5'),
-                (10, 'if_condition_2_end'),
-                ( 3, 'task_b1'),
-                ( 4, 'task_b2'),
-                ( 5, 'concurrence_1_end'),
-                ( 6, 'task_c1'),
-                ( 7, 'task_c2'),
-                ( 8, 'End')]
-
-        assert_same_path(self, path, self.taken_path)
+        if taken_path != expected_path:
+            for taken, expected in zip(taken_path, expected_path):
+                print "TAKEN:   ", taken
+                print "EXPECTED:", expected
+        self.assertEqual(expected_path, taken_path)
 
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(OpenWfeXmlReaderTest)
