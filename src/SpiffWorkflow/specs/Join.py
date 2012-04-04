9fd923a6a0458a3dbee79c402adb3a42b9ded0ec
Samuel Abels <knipknap@gmail.com>
2012-04-04 15:23:30 +0200

    refactor the Join task and add more documentation.

--- src/SpiffWorkflow/specs/Join.py
+++ src/SpiffWorkflow/specs/Join.py
@@ -4,12 +4,12 @@
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -23,6 +23,34 @@ class Join(TaskSpec):
     A task for synchronizing branches that were previously split using a
     conditional task, such as MultiChoice. It has two or more incoming
     branches and one or more outputs.
+
+    Keep in mind that each Join spec may have multiple corresponding
+    Task objects::
+
+        - When using the MultiInstance task
+        - When using the ThreadSplit task
+
+    When using the MultiInstance pattern, Join works across all
+    the resulting task instances. When using the ThreadSplit
+    pattern, Join ignores instances from another thread.
+
+    A Join task may enter the following states::
+
+        - FUTURE, LIKELY, or MAYBE: These are the initial predicted states.
+
+        - WAITING: This state is reached as soon as at least one of the
+        predecessors has completed.
+
+        - READY: All predecessors have completed. If multiple tasks within
+        the thread reference the same Join (e.g. if MultiInstance is used),
+        this state is only reached on one of the tasks; all other tasks go
+        directly from WAITING to completed.
+
+        - COMPLETED: All predecessors have completed, and
+        L{Task.complete()} was called.
+
+    The state may also change directly from WAITING to COMPLETED if the
+    Trigger pattern is used.
     """
 
     def __init__(self,
@@ -34,7 +62,7 @@ class Join(TaskSpec):
                  **kwargs):
         """
         Constructor.
-        
+
         @type  parent: L{SpiffWorkflow.specs.WorkflowSpec}
         @param parent: A reference to the parent (usually a workflow).
         @type  name: string
@@ -61,7 +89,6 @@ class Join(TaskSpec):
         self.threshold        = threshold
         self.cancel_remaining = cancel
 
-
     def _branch_is_complete(self, my_task):
         # Determine whether that branch is now completed by checking whether
         # it has any waiting items other than myself in it.
@@ -75,7 +102,6 @@ class Join(TaskSpec):
                 continue
             return False
         return True
-
 
     def _branch_may_merge_at(self, task):
         for child in task:
@@ -93,25 +119,7 @@ class Join(TaskSpec):
                 return True
         return False
 
-
-    def _fire(self, my_task, waiting_tasks):
-        """
-        Fire, and cancel remaining tasks, if so requested.
-        """
-        # If this is a cancelling join, cancel all incoming branches,
-        # except for the one that just completed.
-        if self.cancel_remaining:
-            for task in waiting_tasks:
-                task.cancel()
-
-
     def _try_fire_unstructured(self, my_task, force = False):
-        # If the threshold was already reached, there is nothing else to do.
-        if my_task._has_state(Task.COMPLETED):
-            return False
-        if my_task._has_state(Task.READY):
-            return True
-
         # The default threshold is the number of inputs.
         threshold = valueof(my_task, self.threshold)
         if threshold is None:
@@ -137,24 +145,9 @@ class Join(TaskSpec):
                 waiting_tasks.append(task)
 
         # If the threshold was reached, get ready to fire.
-        if force or completed >= threshold:
-            self._fire(my_task, waiting_tasks)
-            return True
-
-        # We do NOT set the task state to COMPLETED, because in
-        # case all other incoming tasks get cancelled (or never reach
-        # the Join for other reasons, such as reaching a stub branch), we
-        # we need to revisit it.
-        return False
-
+        return force or completed >= threshold, waiting_tasks
 
     def _try_fire_structured(self, my_task, force = False):
-        # If the threshold was already reached, there is nothing else to do.
-        if my_task._has_state(Task.READY):
-            return True
-        if my_task._has_state(Task.COMPLETED):
-            return False
-
         # Retrieve a list of all activated tasks from the associated
         # task that did the conditional parallel split.
         split_task = my_task._find_ancestor_from_name(self.split_task)
@@ -183,46 +176,94 @@ class Join(TaskSpec):
                 waiting_tasks.append(task)
 
         # If the threshold was reached, get ready to fire.
-        if force or completed >= threshold:
-            self._fire(my_task, waiting_tasks)
-            return True
-
-        # We do NOT set the task state to COMPLETED, because in
-        # case all other incoming tasks get cancelled (or never reach
-        # the Join for other reasons, such as reaching a stub branch), we
-        # need to revisit it.
-        return False
-
+        return force or completed >= threshold, waiting_tasks
 
     def try_fire(self, my_task, force = False):
+        """
+        Checks whether the preconditions for going to READY state are met.
+        Returns True if the threshold was reached, False otherwise.
+        Also returns the list of tasks that yet need to be completed.
+        """
+        # If the threshold was already reached, there is nothing else to do.
+        if my_task._has_state(Task.COMPLETED):
+            return True, None
+        if my_task._has_state(Task.READY):
+            return True, None
+
+        # Check whether we may fire.
         if self.split_task is None:
             return self._try_fire_unstructured(my_task, force)
         return self._try_fire_structured(my_task, force)
 
+    def _update_state_hook(self, my_task):
+        may_fire, waiting_tasks = self.try_fire(my_task)
+        if may_fire:
+            # If this is a cancelling join, cancel all incoming branches,
+            # except for the one that just completed.
+            if self.cancel_remaining:
+                for task in waiting_tasks:
+                    task.cancel()
+
+            # We do NOT set the task state to COMPLETED, because in
+            # case all other incoming tasks get cancelled (or never reach
+            # the Join for other reasons, such as reaching a stub branch),
+            # we need to revisit it.
+            my_task._set_state(Task.READY)
+
+            # Update the state of our child objects.
+            self._do_join(my_task)
+        else:
+            my_task._set_state(Task.WAITING)
 
     def _do_join(self, my_task):
+        # One Join spec may have multiple corresponding Task objects::
+        #
+        #     - Due to the MultiInstance pattern.
+        #     - Due to the ThreadSplit pattern.
+        #
+        # When using the MultiInstance pattern, we want to join across
+        # the resulting task instances. When using the ThreadSplit
+        # pattern, we only join within the same thread. (Both patterns
+        # may also be mixed.)
+        #
+        # We are looking for all task instances that must be joined.
+        # We limit our search by starting at the split point.
         if self.split_task:
             split_task = my_task.workflow.get_task_spec_from_name(self.split_task)
             split_task = my_task._find_ancestor(split_task)
         else:
             split_task = my_task.workflow.task_tree
 
-        # Find the inbound task that was completed last.
+        # Identify all corresponding task instances within the thread.
+        # Also remember which of those instances was most recently changed,
+        # because we are making this one the instance that will
+        # continue the thread of control. In other words, we will continue
+        # to build the task tree underneath the most recently changed task.
         last_changed = None
         thread_tasks = []
         for task in split_task._find_any(self):
+            # Ignore tasks from other threads.
             if task.thread_id != my_task.thread_id:
                 continue
+            # Ignore my outgoing branches.
             if self.split_task and task._is_descendant_of(my_task):
                 continue
+
+            # We have found a matching instance.
+            thread_tasks.append(task)
+
+            # Check whether the state of the instance was recently
+            # changed.
             changed = task.parent.last_state_change
             if last_changed is None \
               or changed > last_changed.parent.last_state_change:
                 last_changed = task
-            thread_tasks.append(task)
 
-        # Mark all tasks in this thread that reference this task as 
-        # completed, except for the first one, which should be READY.
+        # Mark the identified task instances as COMPLETED. The exception
+        # is the most recently changed task, for which we assume READY.
+        # By setting the state to READY only, we allow for calling
+        # L{Task.complete()}, which leads to the task tree being
+        # (re)built underneath the node.
         for task in thread_tasks:
             if task == last_changed:
                 self.entered_event.emit(my_task.workflow, my_task)
@@ -230,8 +271,6 @@ class Join(TaskSpec):
             else:
                 task.state = Task.COMPLETED
                 task._drop_children()
-        return False
-
 
     def _on_trigger(self, my_task):
         """
@@ -241,15 +280,7 @@ class Join(TaskSpec):
         for task in my_task.workflow.task_tree._find_any(self):
             if task.thread_id != my_task.thread_id:
                 continue
-            return self._do_join(task)
-
-
-    def _update_state_hook(self, my_task):
-        if not self.try_fire(my_task):
-            my_task.state = Task.WAITING
-            return False
-        return self._do_join(my_task)
-
+            self._do_join(task)
 
     def _on_complete_hook(self, my_task):
         return TaskSpec._on_complete_hook(self, my_task)
