8b4ed83e21550298eccc0264611101fcffcedf59
Ziad Sawalha <github@highbridgellc.com>
2012-05-28 02:11:26 -0500

    Add Execution Debug Logging in Transform
    - So we can see what commands were executed

--- SpiffWorkflow/specs/Transform.py
+++ SpiffWorkflow/specs/Transform.py
@@ -13,7 +13,11 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+import logging
+
 from SpiffWorkflow.specs.TaskSpec import TaskSpec
+
+LOG = logging.getLogger(__name__)
 
 
 class Transform(TaskSpec):
@@ -46,6 +50,7 @@ class Transform(TaskSpec):
     def _on_complete_hook(self, my_task):
         if self.transforms:
             for transform in self.transforms:
+                LOG.debug("Executing: %s" % transform)
                 exec(transform)
         return TaskSpec._on_complete_hook(self, my_task)
 
