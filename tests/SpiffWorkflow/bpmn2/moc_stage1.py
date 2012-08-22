36ff9499c976e33b566d22ed4cfdbee4b5aee968
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-08-22 16:02:07 +0200

    First test case for bpmn like process.

---
+++ tests/SpiffWorkflow/bpmn2/moc_stage1.py
@@ -0,0 +1,154 @@
+import json
+import unittest
+from SpiffWorkflow.Task import Task
+from SpiffWorkflow.Workflow import TaskIdAssigner, Workflow
+from SpiffWorkflow.operators import Equal, Attrib, Assign
+from SpiffWorkflow.specs.ExclusiveChoice import ExclusiveChoice
+from SpiffWorkflow.specs.Simple import Simple
+from SpiffWorkflow.specs.StartTask import StartTask
+from SpiffWorkflow.specs.WorkflowSpec import WorkflowSpec
+from SpiffWorkflow.storage.DictionarySerializer import DictionarySerializer
+from SpiffWorkflow.storage.JSONSerializer import JSONSerializer
+
+__author__ = 'matth'
+
+class MocStage1Test(unittest.TestCase):
+    def setUp(self):
+        self.spec = self.build_moc_stage1_spec()
+
+    def build_moc_stage1_spec(self):
+        spec     = WorkflowSpec()
+        task1    = Simple(spec, 'prepare-moc-proposal', description='Prepare MOC Proposal')
+        task1.follow(spec.start)
+        task2    = Simple(spec, 'dgm-review', desciption='Review')
+        task2.follow(task1)
+        excl_choice_1 = ExclusiveChoice(spec, 'dgm-review-result')
+        excl_choice_1.follow(task2)
+        task3    = Simple(spec, 'record-on-agenda', desciption='Record on MOC agenda')
+        approve_cond = Equal(Attrib('status'), 'Approve')
+        send_back_cond = Equal(Attrib('status'), 'Send Back (Insufficient Details)')
+        cancel_cond = Equal(Attrib('status'), 'Cancel')
+        excl_choice_1.connect_if(send_back_cond, task1)
+        excl_choice_1.connect_if(cancel_cond, task1)
+        excl_choice_1.connect_if(approve_cond, task3)
+        # We have to have a default:
+        excl_choice_1.connect(task1)
+
+        return spec
+
+    def testMocStage1RunThrough(self):
+
+        self.workflow = Workflow(self.spec)
+
+        self.do_next_exclusive_step('Start')
+        self.do_next_exclusive_step('prepare-moc-proposal')
+        self.do_next_exclusive_step('dgm-review', set_attribs={'status':'Approve'})
+        self.do_next_exclusive_step('dgm-review-result')
+        self.do_next_exclusive_step('record-on-agenda')
+
+    def testMocStage1RunThroughCancel(self):
+
+        self.workflow = Workflow(self.spec)
+
+        self.do_next_exclusive_step('Start')
+        self.do_next_exclusive_step('prepare-moc-proposal')
+        self.do_next_exclusive_step('dgm-review', set_attribs={'status':'Cancel'})
+        self.do_next_exclusive_step('dgm-review-result')
+        self.do_next_exclusive_step('prepare-moc-proposal')
+        self.do_next_exclusive_step('dgm-review', set_attribs={'status':'Approve'})
+        self.do_next_exclusive_step('dgm-review-result')
+        self.do_next_exclusive_step('record-on-agenda')
+
+    def testMocStage1RunThroughSendBack(self):
+
+        self.workflow = Workflow(self.spec)
+
+        self.do_next_exclusive_step('Start')
+
+        for i in range(0,10):
+            self.do_next_exclusive_step('prepare-moc-proposal',with_save_load=False)
+            self.do_next_exclusive_step('dgm-review', set_attribs={'status':'Send Back (Insufficient Details)'},with_save_load=False)
+            self.do_next_exclusive_step('dgm-review-result',with_save_load=False)
+
+        self.do_next_exclusive_step('prepare-moc-proposal',with_save_load=False)
+        self.do_next_exclusive_step('dgm-review', set_attribs={'status':'Send Back (Insufficient Details)'},with_save_load=False)
+        self.do_next_exclusive_step('dgm-review-result',with_save_load=False)
+        self._do_save_and_load()
+
+        self.do_next_exclusive_step('prepare-moc-proposal')
+        self.do_next_exclusive_step('dgm-review', set_attribs={'status':'Approve'})
+        self.do_next_exclusive_step('dgm-review-result')
+        self.do_next_exclusive_step('record-on-agenda')
+
+
+    def test_simple_save_load(self):
+        self.workflow = Workflow(self.spec)
+
+        self.do_next_exclusive_step('Start')
+        self.do_next_exclusive_step('prepare-moc-proposal')
+
+        state = self.workflow.serialize(JSONSerializer())
+
+        old_workflow = self.workflow
+        self.workflow = JSONSerializer().deserialize_workflow(state)
+        new_state = self.workflow.serialize(JSONSerializer())
+        self.assertEqual(json.dumps(json.loads(state), sort_keys=True), json.dumps(json.loads(new_state), sort_keys=True))
+
+        #self.do_next_exclusive_step('dgm-review', set_attribs={'status':'Approve'})
+        tasks = self.workflow.get_tasks(Task.READY)
+        self.assertEqual(len(tasks), 1)
+        self.assertEqual(tasks[0].task_spec.name, 'dgm-review')
+
+        old_tasks = old_workflow.get_tasks(Task.READY)
+        self.assertEqual(len(old_tasks), 1)
+        self.assertEqual(old_tasks[0].task_spec.name, 'dgm-review')
+
+        tasks[0].set_attribute(**{'status':'Approve'})
+        old_tasks[0].set_attribute(**{'status':'Approve'})
+
+        print hex(id(old_tasks[0].children[0].parent)), old_tasks[0].children[0].parent.id
+        print hex(id(old_tasks[0])), old_tasks[0].id
+        print hex(id(tasks[0].children[0].parent)), tasks[0].children[0].parent.id
+        print hex(id(tasks[0])), tasks[0].id
+
+        old_workflow.complete_task_from_id(old_tasks[0].id)
+        self.workflow.complete_task_from_id(tasks[0].id)
+
+        t = self.workflow.get_tasks(Task.READY)
+        t2 = old_workflow.get_tasks(Task.READY)
+
+        self.do_next_exclusive_step('dgm-review-result')
+        self.do_next_exclusive_step('record-on-agenda')
+
+    def do_next_exclusive_step(self, step_name, with_save_load=True, set_attribs=None):
+
+        old_workflow = self.workflow
+        if with_save_load:
+            self._do_save_and_load()
+
+        tasks = self.workflow.get_tasks(Task.READY)
+        self.assertEqual(len(tasks), 1)
+        self.assertEqual(tasks[0].task_spec.name, step_name)
+        if set_attribs:
+            tasks[0].set_attribute(**set_attribs)
+        self.workflow.complete_task_from_id(tasks[0].id)
+
+    def _do_save_and_load(self):
+        state = self.workflow.serialize(JSONSerializer())
+        new_workflow = JSONSerializer().deserialize_workflow(state)
+        new_state = new_workflow.serialize(JSONSerializer())
+        pretty_state = json.dumps(json.loads(state), sort_keys=True, indent=4)
+        pretty_new_state = json.dumps(json.loads(new_state), sort_keys=True, indent=4)
+        self.assertEqual(pretty_state, pretty_new_state)
+        self.assertEquals(len(self.workflow.get_tasks(Task.READY)), len(new_workflow.get_tasks(Task.READY)))
+        self.assertEquals(self.workflow.get_tasks(Task.READY)[0].id, new_workflow.get_tasks(Task.READY)[0].id)
+        self.assertEquals(self.workflow.get_tasks(Task.READY)[0].task_spec.name, new_workflow.get_tasks(Task.READY)[0].task_spec.name)
+        self.workflow = new_workflow
+
+
+
+
+def suite():
+    return unittest.TestLoader().loadTestsFromTestCase(MocStage1Test)
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity = 2).run(suite())
