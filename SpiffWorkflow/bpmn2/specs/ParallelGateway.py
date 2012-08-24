832d303d41cfa8d8994fed16c1756df4a9afff50
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-08-24 18:00:47 +0200

    Adding parallel gateway.

---
+++ SpiffWorkflow/bpmn2/specs/ParallelGateway.py
@@ -0,0 +1,7 @@
+from SpiffWorkflow.bpmn2.specs.BpmnSpecMixin import BpmnSpecMixin
+from SpiffWorkflow.specs.Simple import Simple
+
+__author__ = 'matth'
+
+class ParallelGateway(Simple, BpmnSpecMixin):
+    pass
