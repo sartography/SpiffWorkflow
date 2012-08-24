e820f4a7479046bf940d39613c17dd20a9b6cb4f
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-08-24 16:37:41 +0200

    Added support for passing the end state name as a choice from a subprocess.

--- SpiffWorkflow/bpmn2/specs/CallActivity.py
+++ SpiffWorkflow/bpmn2/specs/CallActivity.py
@@ -1,4 +1,5 @@
 from SpiffWorkflow.bpmn2.specs.BpmnSpecMixin import BpmnSpecMixin
+from SpiffWorkflow.operators import Assign
 from SpiffWorkflow.specs.SubWorkflow import SubWorkflow
 from SpiffWorkflow.specs.TaskSpec import TaskSpec
 
@@ -7,7 +8,7 @@ __author__ = 'matth'
 class CallActivity(SubWorkflow, BpmnSpecMixin):
 
     def __init__(self, parent, name, wf_spec, **kwargs):
-        super(CallActivity, self).__init__(parent, name, None, **kwargs)
+        super(CallActivity, self).__init__(parent, name, None, out_assign=[Assign('choice', 'choice')], **kwargs)
         self.spec = wf_spec
 
     def test(self):
--- SpiffWorkflow/bpmn2/specs/UserTask.py
+++ SpiffWorkflow/bpmn2/specs/UserTask.py
@@ -7,11 +7,10 @@ __author__ = 'matth'
 class UserTask(Simple, BpmnSpecMixin):
 
     def get_user_choices(self):
+        if not self.outputs:
+            return []
         assert len(self.outputs) == 1
         next_node = self.outputs[0]
         if isinstance(next_node, ExclusiveGateway):
             return next_node.get_outgoing_sequence_names()
         return self.get_outgoing_sequence_names()
-
-
-
