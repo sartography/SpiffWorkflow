6af4d82e3dce55b145b2530d7153bc2b427a7f8f
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-08-24 08:51:33 +0200

    Make it possible to extend SubWorkflow to provide a spec programatically.

--- SpiffWorkflow/specs/SubWorkflow.py
+++ SpiffWorkflow/specs/SubWorkflow.py
@@ -81,14 +81,16 @@ class SubWorkflow(TaskSpec):
         else:
             my_task._update_children(outputs, Task.FUTURE)
 
-
-    def _on_ready_before_hook(self, my_task):
+    def _get_workflow_spec(self):
         from SpiffWorkflow.storage import XmlSerializer
         from SpiffWorkflow.specs import WorkflowSpec
         file           = valueof(my_task, self.file)
         serializer     = XmlSerializer()
         xml            = open(file).read()
-        wf_spec        = WorkflowSpec.deserialize(serializer, xml, filename = file)
+        return WorkflowSpec.deserialize(serializer, xml, filename = file)
+
+    def _on_ready_before_hook(self, my_task):
+        wf_spec        = self._get_workflow_spec()
         outer_workflow = my_task.workflow.outer_workflow
         subworkflow    = SpiffWorkflow.Workflow(wf_spec, parent = outer_workflow)
         subworkflow.completed_event.connect(self._on_subworkflow_completed, my_task)
@@ -103,7 +105,6 @@ class SubWorkflow(TaskSpec):
 
         my_task._set_internal_attribute(subworkflow = subworkflow)
         return True
-
 
     def _on_ready_hook(self, my_task):
         # Assign variables, if so requested.
