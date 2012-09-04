b9a2cfa332d96e516b42bc505471d4030032affb
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-09-04 12:44:22 +0200

    Adding a non-interrupting event on a subprocess.

--- tests/SpiffWorkflow/bpmn2/test_workflows.py
+++ tests/SpiffWorkflow/bpmn2/test_workflows.py
@@ -198,6 +198,121 @@ class MessageInterruptsSpTest(WorkflowTest):
         self.workflow.do_engine_steps()
         self.assertEquals(0, len(self.workflow.get_tasks(Task.READY | Task.WAITING)))
 
+
+class MessageNonInterruptsSpTest(WorkflowTest):
+    def setUp(self):
+        self.spec = self.load_spec()
+
+    def load_spec(self):
+        return self.load_workflow_spec('Test-Workflows/*.bpmn20.xml', 'Message Non Interrupt SP')
+
+    def testRunThroughHappySaveAndRestore(self):
+
+        self.workflow = BpmnWorkflow(self.spec)
+        self.save_restore()
+
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.assertEquals(1, len(self.workflow.get_tasks(Task.READY)))
+        self.assertEquals(1, len(self.workflow.get_tasks(Task.WAITING)))
+
+        self.do_next_exclusive_step('Do Something In a Subprocess')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.do_next_exclusive_step('Ack Subprocess Done')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.workflow.do_engine_steps()
+        self.assertEquals(0, len(self.workflow.get_tasks(Task.READY | Task.WAITING)))
+
+    def testRunThroughMessageSaveAndRestore(self):
+
+        self.workflow = BpmnWorkflow(self.spec)
+        self.save_restore()
+
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.assertEquals(1, len(self.workflow.get_tasks(Task.READY)))
+        self.assertEquals(1, len(self.workflow.get_tasks(Task.WAITING)))
+
+        self.workflow.accept_message('Test Message')
+
+        self.do_next_named_step('Do Something In a Subprocess')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.do_next_named_step('Ack Subprocess Done')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.do_next_named_step('Acknowledge SP Parallel Message')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.workflow.do_engine_steps()
+        self.assertEquals(0, len(self.workflow.get_tasks(Task.READY | Task.WAITING)))
+
+    def testRunThroughMessageOrder2SaveAndRestore(self):
+
+        self.workflow = BpmnWorkflow(self.spec)
+        self.save_restore()
+
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.assertEquals(1, len(self.workflow.get_tasks(Task.READY)))
+        self.assertEquals(1, len(self.workflow.get_tasks(Task.WAITING)))
+
+        self.workflow.accept_message('Test Message')
+
+        self.do_next_named_step('Do Something In a Subprocess')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.do_next_named_step('Acknowledge SP Parallel Message')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.do_next_named_step('Ack Subprocess Done')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.workflow.do_engine_steps()
+        self.assertEquals(0, len(self.workflow.get_tasks(Task.READY | Task.WAITING)))
+
+    def testRunThroughMessageOrder3SaveAndRestore(self):
+
+        self.workflow = BpmnWorkflow(self.spec)
+        self.save_restore()
+
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.assertEquals(1, len(self.workflow.get_tasks(Task.READY)))
+        self.assertEquals(1, len(self.workflow.get_tasks(Task.WAITING)))
+
+        self.workflow.accept_message('Test Message')
+
+        self.do_next_named_step('Acknowledge SP Parallel Message')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.do_next_named_step('Do Something In a Subprocess')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.do_next_named_step('Ack Subprocess Done')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+
+        self.workflow.do_engine_steps()
+        self.assertEquals(0, len(self.workflow.get_tasks(Task.READY | Task.WAITING)))
+
+
 class MessageNonInterruptTest(WorkflowTest):
     def setUp(self):
         self.spec = self.load_spec()
