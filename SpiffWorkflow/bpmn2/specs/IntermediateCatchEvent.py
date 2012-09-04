062e69fb98bcc2746fae4d2f62a0a82df2ecfb8a
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-09-04 14:31:10 +0200

    Adding hook method when entering a WAITING state.

--- SpiffWorkflow/bpmn2/specs/IntermediateCatchEvent.py
+++ SpiffWorkflow/bpmn2/specs/IntermediateCatchEvent.py
@@ -10,13 +10,17 @@ class IntermediateCatchEvent(Simple, BpmnSpecMixin):
         super(IntermediateCatchEvent, self).__init__(parent, name, **kwargs)
         self.event_spec = event_spec
 
-
     def _update_state_hook(self, my_task):
         if self.event_spec.has_fired(my_task):
             return super(IntermediateCatchEvent, self)._update_state_hook(my_task)
         else:
-            my_task._set_state(Task.WAITING)
+            if not my_task.state == Task.WAITING:
+                my_task._set_state(Task.WAITING)
+                self._on_enter_waiting_state(my_task)
             return False
+
+    def _on_enter_waiting_state(self, my_task):
+        pass
 
     def accept_message(self, my_task, message):
         if my_task.state == Task.WAITING and self.event_spec.accept_message(my_task, message):
