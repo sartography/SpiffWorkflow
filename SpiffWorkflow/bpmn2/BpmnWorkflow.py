558a7b198423c9922abe724bd3754cde70d2562c
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-09-10 10:15:50 +0200

    Make evaluation of expressions more useful.

--- SpiffWorkflow/bpmn2/BpmnWorkflow.py
+++ SpiffWorkflow/bpmn2/BpmnWorkflow.py
@@ -148,10 +148,14 @@ class BpmnCondition(Operator):
 class BpmnScriptEngine(object):
 
     def evaluate(self, task, expression):
-        if isinstance(expression, basestring):
-            return task.get_attribute(expression, None)
-        else:
+        if isinstance(expression, Operator):
             return expression._matches(task)
+        else:
+            return self._eval(task, expression, **task.get_attributes())
+
+    def _eval(self, task, expression, **kwargs):
+        locals().update(kwargs)
+        return eval(expression)
 
     def execute(self, task, script):
         exec script
