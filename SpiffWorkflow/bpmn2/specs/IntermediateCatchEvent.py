5f370a75d4e812e6bf63682c7a22652f422abb93
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-09-19 08:11:43 +0200

    The IntermediateCatchEvent needs to predict children on ready, and should not enter the waiting state if the parent is not yet finished.

--- SpiffWorkflow/bpmn2/specs/IntermediateCatchEvent.py
+++ SpiffWorkflow/bpmn2/specs/IntermediateCatchEvent.py
@@ -13,13 +13,17 @@ class IntermediateCatchEvent(Simple, BpmnSpecMixin):
     def _update_state_hook(self, my_task):
         target_state = getattr(my_task, '_bpmn_load_target_state', None)
         if target_state == Task.READY or (not my_task.workflow.is_busy_with_restore() and self.event_spec.has_fired(my_task)):
-            my_task._set_state(Task.LIKELY)
             super(IntermediateCatchEvent, self)._update_state_hook(my_task)
         else:
+            if not my_task.parent._is_finished():
+                return
             if not my_task.state == Task.WAITING:
                 my_task._set_state(Task.WAITING)
                 if not my_task.workflow.is_busy_with_restore():
                     self.entering_waiting_state(my_task)
+
+    def _on_ready_hook(self, my_task):
+        self._predict(my_task)
 
     def accept_message(self, my_task, message):
         if my_task.state == Task.WAITING and self.event_spec.accept_message(my_task, message):
