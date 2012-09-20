89193cfbe37d0ca5c6168053c69574dceeeef4db
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-09-20 09:37:44 +0200

    Specify the editor explicitly.

--- SpiffWorkflow/bpmn/storage/Packager.py
+++ SpiffWorkflow/bpmn/storage/Packager.py
@@ -16,13 +16,14 @@ class Packager(object):
 
     PARSER_CLASS = BpmnParser
 
-    def __init__(self, package_file, entry_point_process, meta_data=None):
+    def __init__(self, package_file, entry_point_process, meta_data=None, editor=None):
         self.package_file = package_file
         self.entry_point_process = entry_point_process
         self.parser = self.PARSER_CLASS()
         self.meta_data = meta_data or []
         self.input_files = []
         self.input_path_prefix = None
+        self.editor = editor
 
     def add_bpmn_file(self, filename):
         self.add_bpmn_files([filename])
@@ -52,45 +53,54 @@ class Packager(object):
         self.wf_spec = self.parser.get_spec(self.entry_point_process)
 
         #Now package everything:
-        package_zip = zipfile.ZipFile(self.package_file, "w", compression=zipfile.ZIP_DEFLATED)
+        self.package_zip = zipfile.ZipFile(self.package_file, "w", compression=zipfile.ZIP_DEFLATED)
 
-        self.write_meta_data(package_zip)
+        self.write_meta_data()
 
         done_files = set()
         for spec in self.wf_spec.get_specs_depth_first():
             filename = spec.file
             if not filename in done_files:
                 done_files.add(filename)
-                package_zip.write(filename, "%s.bpmn" % spec.name)
+                self.package_zip.write(filename, "%s.bpmn" % spec.name)
 
-                package_zip.write(filename, "src/" + self._get_zip_path(filename))
+                self.package_zip.write(filename, "src/" + self._get_zip_path(filename))
 
-                if filename.endswith('.bpmn20.xml'):
-                    signavio_file = filename[:-len('.bpmn20.xml')] + '.signavio.xml'
-                    if os.path.exists(signavio_file):
-                        package_zip.write(signavio_file, "src/" + self._get_zip_path(signavio_file))
+                self._call_editor_hook('package_for_editor', spec, filename)
 
-                        f = open(signavio_file, 'r')
-                        try:
-                            signavio_tree = etree.parse(f)
-                        finally:
-                            f.close()
-                        svg_node = one(signavio_tree.xpath('.//svg-representation'))
-                        svg = etree.fromstring(svg_node.text)
-                        package_zip.writestr("%s.svg" % spec.name, etree.tostring(svg,pretty_print=True))
+    def _call_editor_hook(self, hook, *args, **kwargs):
+        if self.editor:
+            hook_func = getattr(self, "%s_%s" % (hook, self.editor), None)
+            if hook_func:
+                hook_func(*args, **kwargs)
 
+    def package_for_editor_signavio(self, spec, filename):
+        signavio_file = filename[:-len('.bpmn20.xml')] + '.signavio.xml'
+        if os.path.exists(signavio_file):
+            self.package_zip.write(signavio_file, "src/" + self._get_zip_path(signavio_file))
 
-    def write_meta_data(self, package_zip):
+            f = open(signavio_file, 'r')
+            try:
+                signavio_tree = etree.parse(f)
+            finally:
+                f.close()
+            svg_node = one(signavio_tree.xpath('.//svg-representation'))
+            svg = etree.fromstring(svg_node.text)
+            self.package_zip.writestr("%s.svg" % spec.name, etree.tostring(svg,pretty_print=True))
+
+    def write_meta_data(self):
         config = ConfigParser.SafeConfigParser()
 
         config.add_section('MetaData')
         config.set('MetaData', 'entry_point_process', self.wf_spec.name)
+        if self.editor:
+            config.set('MetaData', 'editor', self.editor)
         for k, v in self.meta_data:
             config.set('MetaData', k, v)
 
         ini = StringIO()
         config.write(ini)
-        package_zip.writestr("metadata.ini", ini.getvalue())
+        self.package_zip.writestr("metadata.ini", ini.getvalue())
 
     def _get_zip_path(self, filename):
         p = os.path.abspath(filename)[len(self.input_path_prefix):].replace(os.path.sep, '/')
@@ -112,6 +122,12 @@ def main(packager_class=None):
         help="create the BPMN package in the specified file")
     parser.add_option("-p", "--process", dest="entry_point_process",
         help="specify the entry point process")
+
+    group = OptionGroup(parser, "BPMN Editor Options",
+        "These options are not required, but may be provided to activate special features of supported BPMN editors.")
+    group.add_option("--editor", dest="editor",
+        help="editors with special support: signavio")
+    parser.add_option_group(group)
 
     group = OptionGroup(parser, "Target Engine Options",
         "These options are not required, but may be provided if a specific BPMN application engine is targeted.")
@@ -144,7 +160,7 @@ def main(packager_class=None):
     if options.target_engine:
         meta_data.append(('target_engine_version', options.target_engine_version))
 
-    packager = packager_class(package_file=options.package_file, entry_point_process=options.entry_point_process, meta_data=meta_data)
+    packager = packager_class(package_file=options.package_file, entry_point_process=options.entry_point_process, meta_data=meta_data, editor=options.editor)
     for a in args:
         packager.add_bpmn_files_by_glob(a)
     packager.create_package()
