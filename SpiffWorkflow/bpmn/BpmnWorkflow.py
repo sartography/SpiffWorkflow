330a5dd1a447eb539084507e0e9e5baa18b454e1
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-09-20 13:06:04 +0200

    Move script engine in to it's own file.

--- SpiffWorkflow/bpmn/BpmnWorkflow.py
+++ SpiffWorkflow/bpmn/BpmnWorkflow.py
@@ -1,26 +1,9 @@
 from SpiffWorkflow.Task import Task
 from SpiffWorkflow.Workflow import Workflow
+from SpiffWorkflow.bpmn.BpmnScriptEngine import BpmnScriptEngine
 from SpiffWorkflow.bpmn.storage.MinimalistWorkflowSerializer import _BpmnProcessSpecState
-from SpiffWorkflow.operators import Operator
 
 __author__ = 'matth'
-
-
-
-class BpmnScriptEngine(object):
-
-    def evaluate(self, task, expression):
-        if isinstance(expression, Operator):
-            return expression._matches(task)
-        else:
-            return self._eval(task, expression, **task.get_attributes())
-
-    def _eval(self, task, expression, **kwargs):
-        locals().update(kwargs)
-        return eval(expression)
-
-    def execute(self, task, script):
-        exec script
 
 class BpmnWorkflow(Workflow):
 
