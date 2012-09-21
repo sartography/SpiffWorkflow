e5136ecff976bc2e1db019d85728f13683101717
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-09-21 06:52:40 +0200

    Updating API docs.

--- SpiffWorkflow/bpmn/BpmnWorkflow.py
+++ SpiffWorkflow/bpmn/BpmnWorkflow.py
@@ -5,8 +5,22 @@ from SpiffWorkflow.bpmn.BpmnScriptEngine import BpmnScriptEngine
 __author__ = 'matth'
 
 class BpmnWorkflow(Workflow):
+    """
+    The engine that executes a BPMN workflow. This specialises the standard Spiff Workflow class
+    with a few extra methods and attributes.
+    """
 
     def __init__(self, workflow_spec, name=None, script_engine=None, read_only=False, **kwargs):
+        """
+        Constructor.
+
+        :param script_engine: set to an instance of BpmnScriptEngine if you need a specialised
+        version. Defaults to a new BpmnScriptEngine instance.
+
+        :param read_only: If this parameter is set then the workflow state cannot change. It
+        can only be queried to find out about the current state. This is used in conjunction with
+        the CompactWorkflowSerializer to provide read only access to a previously saved workflow.
+        """
         super(BpmnWorkflow, self).__init__(workflow_spec, **kwargs)
         self.name = name or workflow_spec.name
         self.script_engine = script_engine or BpmnScriptEngine()
@@ -14,6 +28,10 @@ class BpmnWorkflow(Workflow):
         self.read_only = read_only
 
     def accept_message(self, message):
+        """
+        Indicate to the workflow that a message has been received. The message will be processed
+        by any waiting Intermediate or Boundary Message Events, that are waiting for the message.
+        """
         assert not self.read_only
         self.refresh_waiting_tasks()
         self.do_engine_steps()
@@ -21,6 +39,11 @@ class BpmnWorkflow(Workflow):
             my_task.task_spec.accept_message(my_task, message)
 
     def do_engine_steps(self):
+        """
+        Execute any READY tasks that are engine specific (for example, gateways or script tasks).
+        This is done in a loop, so it will keep completing those tasks until there are only
+        READY User tasks, or WAITING tasks left.
+        """
         assert not self.read_only
         engine_steps = filter(lambda t: self._is_engine_task(t.task_spec), self.get_tasks(Task.READY))
         while engine_steps:
@@ -29,14 +52,24 @@ class BpmnWorkflow(Workflow):
             engine_steps = filter(lambda t: self._is_engine_task(t.task_spec), self.get_tasks(Task.READY))
 
     def refresh_waiting_tasks(self):
+        """
+        Refresh the state of all WAITING tasks. This will, for example, update Catching Timer Events
+        whose waiting time has passed.
+        """
         assert not self.read_only
         for my_task in self.get_tasks(Task.WAITING):
             my_task.task_spec._update_state(my_task)
 
     def get_ready_user_tasks(self):
+        """
+        Returns a list of User Tasks that are READY for user action
+        """
         return filter(lambda t: not self._is_engine_task(t.task_spec), self.get_tasks(Task.READY))
 
     def get_waiting_tasks(self):
+        """
+        Returns a list of all WAITING tasks
+        """
         return self.get_tasks(Task.WAITING)
 
     def _is_busy_with_restore(self):
