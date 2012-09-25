3553e8fb6e7f7db157c70cc823e6a5fd7721618b
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-09-25 10:48:46 +0200

    Pass kwargs through to the new_workflow method.

--- SpiffWorkflow/bpmn/storage/CompactWorkflowSerializer.py
+++ SpiffWorkflow/bpmn/storage/CompactWorkflowSerializer.py
@@ -215,14 +215,23 @@ class CompactWorkflowSerializer(Serializer):
         :param s_state: the state of the workflow as returned by serialize_workflow
         :param workflow_spec: the Workflow Spec of the workflow (CompactWorkflowSerializer only supports workflow serialization)
         :param read_only: (Optional) True if the workflow should be restored in READ ONLY mode
+
+        NB: Additional kwargs passed to the deserialize_workflow method will be passed to the new_workflow method.
         """
         if workflow_spec is None:
             raise NotImplementedError('Including the spec serialization with the workflow state is not implemented. A \'workflow_spec\' must be provided.')
-        workflow = self.new_workflow(workflow_spec, read_only=read_only)
+        workflow = self.new_workflow(workflow_spec, read_only=read_only, **kwargs)
         self._restore_workflow_state(workflow, s_state)
         return workflow
 
-    def new_workflow(self, workflow_spec, read_only=False):
+    def new_workflow(self, workflow_spec, read_only=False, **kwargs):
+        """
+        Create a new workflow instance from the given spec and arguments.
+
+        :param workflow_spec: the workflow spec to use
+        :param read_only: this should be in read only mode
+        :param kwargs: Any extra kwargs passed to the deserialize_workflow method will be passed through here
+        """
         return BpmnWorkflow(workflow_spec, read_only=read_only)
 
     def _get_workflow_state(self, workflow):
