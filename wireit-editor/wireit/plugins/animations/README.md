106e1f989e7e59a2d402d117999e1860c336291b
Samuel <knipknap@gmail.com>
2012-10-15 15:18:05 -0400

    get rid of the incomplete Javascript editor; this is now in a new project called flowcanvas.

--- wireit-editor/wireit/plugins/animations/README.md
+++
@@ -1,7 +0,0 @@
-# WireIt animations plugin
-
-
- * Utility to provide Animation on any DOM element containing terminals
-
- * clearExplode on Layer
-
--- wireit-editor/wireit/plugins/inputex/lib/inputex/examples/default.php
+++
@@ -1,7 +0,0 @@
-<?php
-   sleep(1);
-   $jsonValue = stripslashes($_POST["value"]);
-   //echo $jsonValue."\n";
-   $obj = json_decode($jsonValue);
-   print_r($obj);
-?>
