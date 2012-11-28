af652a930c42056a482c02253bcabb642d0bd232
Matthew Hampton <matthew.j.hampton@gmail.com>
2012-11-28 12:04:28 +0200

    Another parallel test.

--- tests/SpiffWorkflow/bpmn/ParallelTest.py
+++ tests/SpiffWorkflow/bpmn/ParallelTest.py
@@ -65,6 +65,40 @@ class ParallelJoinLongTest(BpmnWorkflowTestCase):
 
         self.assertEquals(0, len(self.workflow.get_tasks(Task.READY | Task.WAITING)))
 
+class ParallelMultipleSplitsTest(BpmnWorkflowTestCase):
+    def setUp(self):
+        self.spec = self.load_spec()
+
+    def load_spec(self):
+        return self.load_workflow_spec('Test-Workflows/Parallel-Multiple-Splits.bpmn20.xml', 'Parallel Multiple Splits')
+
+    def testRunThroughAlternating(self):
+
+        self.workflow = BpmnWorkflow(self.spec)
+        self.workflow.do_engine_steps()
+
+        self.assertEquals(2, len(self.workflow.get_tasks(Task.READY)))
+
+        self.do_next_named_step('Do First')
+        self.workflow.do_engine_steps()
+        self.do_next_named_step('SP 1 - Choose', choice='Yes')
+        self.workflow.do_engine_steps()
+        self.do_next_named_step('SP 2 - Choose', choice='Yes')
+        self.workflow.do_engine_steps()
+        self.do_next_named_step('SP 3 - Choose', choice='Yes')
+        self.workflow.do_engine_steps()
+        self.do_next_named_step('SP 1 - Yes Task')
+        self.workflow.do_engine_steps()
+        self.do_next_named_step('SP 2 - Yes Task')
+        self.workflow.do_engine_steps()
+        self.do_next_named_step('SP 3 - Yes Task')
+        self.workflow.do_engine_steps()
+
+        self.do_next_named_step('Done')
+        self.workflow.do_engine_steps()
+
+        self.assertEquals(0, len(self.workflow.get_tasks(Task.READY | Task.WAITING)))
+
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(ParallelJoinLongTest)
 if __name__ == '__main__':
