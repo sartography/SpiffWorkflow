ee84959904e03783f79be2d0244b60e10cc6be13
Samuel <knipknap@gmail.com>
2017-04-28 08:27:43 +0200

    Update deserialize.py
    Fix tutorial bug

--- doc/tutorial/deserialize.py
+++ doc/tutorial/deserialize.py
@@ -4,4 +4,4 @@ from SpiffWorkflow.serializer.json import JSONSerializer
 serializer = JSONSerializer()
 with open('workflow-spec.json') as fp:
     workflow_json = fp.read()
-spec = WorkflowSpec.deserialize(serializer, workflow_json, 'nuclear.json')
+spec = WorkflowSpec.deserialize(serializer, workflow_json)
