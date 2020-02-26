a36964f635538b5d4ff14acbc9784a12583f5baa
robocad <github@ngist.net>
2020-02-26 14:23:11 +0100

    Update workflow.py
    small fix of return type hint for get_tasks_from_spec_name

--- SpiffWorkflow/workflow.py
+++ SpiffWorkflow/workflow.py
@@ -176,8 +176,8 @@ class Workflow(object):
 
         :type name: str
         :param name: The name of a task spec.
-        :rtype: Task
-        :return: The task that relates to the spec with the given name.
+        :rtype: list[Task]
+        :returns: A list of tasks that relate to the spec with the given name.
         """
         return [task for task in self.get_tasks()
                 if task.task_spec.name == name]
