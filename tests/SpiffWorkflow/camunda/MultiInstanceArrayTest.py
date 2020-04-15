1d10c1faf9c0449423f707f627511f1e31e26b79
Dan Funk <daniel.h.funk@gmail.com>
2020-04-15 15:24:36 -0400

    Just some nonsense, and general brokenness.  But Kelly can fix anything.

--- tests/SpiffWorkflow/camunda/MultiInstanceArrayTest.py
+++ tests/SpiffWorkflow/camunda/MultiInstanceArrayTest.py
@@ -34,7 +34,7 @@ class MultiInstanceArrayTest(BaseTestCase):
         self.actual_test2(True)
 
 
-        
+
     def actual_test(self, save_restore=False):
 
         self.workflow = BpmnWorkflow(self.spec)
@@ -43,7 +43,7 @@ class MultiInstanceArrayTest(BaseTestCase):
         # Set initial array size to 3 in the first user form.
         task = self.workflow.get_ready_user_tasks()[0]
         self.assertEquals("Activity_FamSize", task.task_spec.name)
-        task.update_data({"FamilySize": 3})
+        task.update_data({"Family": {"Size": 3}})
         self.workflow.complete_task_from_id(task.id)
         if save_restore: self.save_restore()
 
@@ -58,7 +58,7 @@ class MultiInstanceArrayTest(BaseTestCase):
         self.assertEqual({1: {'FirstName': 'The Funk #0'},
                           2: {'FirstName': 'The Funk #1'},
                           3: {'FirstName': 'The Funk #2'}},
-                         task.data["FamilyMembers"])
+                         task.data["Family"]["Members"])
         # Set the birthdays of the 3 family members.
         for i in range(3):
             task = self.workflow.get_ready_user_tasks()[0]
@@ -103,24 +103,24 @@ class MultiInstanceArrayTest(BaseTestCase):
                           2: {'FirstName': 'The Funk #1'},
                           3: {'FirstName': 'The Funk #2'}},
                          task.data["FamilyMembers"])
-        
-        
+
+
         # Make sure that if we have a list as both input and output
         # collection, that we raise an exception
-        
+
         task = self.workflow.get_ready_user_tasks()[0]
         task.data['FamilyMembers'] = ['The Funk #0','The Funk #1','The Funk #2']
         self.assertEquals("FamilyMemberBday", task.task_spec.name)
         task.update_data({"Birthdate": "10/0%i/1985" % i})
         with self.assertRaises(WorkflowException) as context:
             self.workflow.complete_task_from_id(task.id)
-            
 
 
 
 
 
-        
+
+
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(MultiInstanceArrayTest)
 
