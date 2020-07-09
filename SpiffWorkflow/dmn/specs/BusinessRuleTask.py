ffac9460719116a9b23438c87ad5189014701da5
Kelly McDonald <kelly@fammcdonald.net>
2020-07-09 09:31:47 -0400

    Add a workflowTaskExecException on the task if we have a syntax error in the script for the decision rule

--- SpiffWorkflow/dmn/specs/BusinessRuleTask.py
+++ SpiffWorkflow/dmn/specs/BusinessRuleTask.py
@@ -28,4 +28,7 @@ class BusinessRuleTask(Simple, BpmnSpecMixin):
         if self.res is not None: # it is conceivable that no rules would fire.
             self.resDict = self.res.outputAsDict(my_task.data)
             my_task.data.update(self.resDict)
-        super(BusinessRuleTask, self)._on_complete_hook(my_task)
+        try:
+            super(BusinessRuleTask, self)._on_complete_hook(my_task)
+        except Exception as e:
+            raise WorkflowTaskExecException(my_task, str(e))
