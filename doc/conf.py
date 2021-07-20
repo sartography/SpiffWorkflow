310d5d10ebfa7da4891d81e8c6a36e7ba287b174
Dan <daniel.h.funk@gmail.com>
2021-07-20 12:02:52 -0400

    trying like hell to get the docs to build on readthedocs.

--- doc/conf.py
+++ doc/conf.py
@@ -57,3 +57,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Set the master index file.
+master_doc = 'index'
