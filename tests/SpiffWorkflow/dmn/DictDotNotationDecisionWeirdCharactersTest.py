35e785738bfd5d87564f160390881772e99804a9
Aaron Louie <ajlouie@gmail.com>
2021-08-19 17:21:08 -0400

    Tweaks the xpath so it can handle the DMN v1.3 XML that Camunda's editor generates. Adds some v1.3 DMN files and tests that they can be parsed.

--- tests/SpiffWorkflow/dmn/DictDotNotationDecisionWeirdCharactersTest.py
+++ tests/SpiffWorkflow/dmn/DictDotNotationDecisionWeirdCharactersTest.py
@@ -8,15 +8,20 @@ class DictDotNotationDecisionWeirdCharactersTestClass(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.runner = DecisionRunner('dict_dot_notation_decision_weird_characters.dmn', debug='DEBUG')
+        dmn_files =[
+            'dict_dot_notation_decision_weird_characters.dmn',
+            'dict_dot_notation_decision_weird_characters_v1_3.dmn',
+        ]
+        cls.runners = [DecisionRunner(d, debug='DEBUG') for d in dmn_files]
 
     def test_string_decision_string_output1(self):
-        data = {"odd_foods": {
-            "SPAM_LIKE": {"delicious": False}
-        }}
-        res = self.runner.decide(Box(data))
-        self.assertEqual(res.description, 'This person has a tongue, brain '
-                                          'or sense of smell.')
+        for runner in self.runners:
+            data = {"odd_foods": {
+                "SPAM_LIKE": {"delicious": False}
+            }}
+            res = runner.decide(Box(data))
+            self.assertEqual(res.description, 'This person has a tongue, brain '
+                                              'or sense of smell.')
 
     data = {"foods": {
         "spam": {"delicious": False}
