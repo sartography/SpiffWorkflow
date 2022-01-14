f100db6476bc5448bfb8207d518aee5c86e7a049
Dan <daniel.h.funk@gmail.com>
2022-01-14 12:41:56 -0500

    Incrementing version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(name='SpiffWorkflow',
-      version='1.1.2',
+      version='1.1.4',
       description='A workflow framework and BPMN/DMN Processor',
       long_description=README,
       long_description_content_type="text/markdown",
