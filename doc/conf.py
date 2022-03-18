88b86359d4c04a19cb5342196533d14ab4ed84a2
Dan <daniel.h.funk@gmail.com>
2022-03-17 20:15:05 -0400

    Bumping version.

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Sartography'
 author = 'Sartography'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.4'
+release = '1.1.5'
 
 
 # -- General configuration ---------------------------------------------------
