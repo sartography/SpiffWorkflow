56dc45ffddbda4810868f3336d6a0d1686818a73
jbirddog <jon.herron@yahoo.com>
2022-08-19 13:10:31 -0400

    Some test setup, tweak node names again

--- tests/SpiffWorkflow/bpmn/ServiceTaskTest.py
+++ tests/SpiffWorkflow/bpmn/ServiceTaskTest.py
@@ -7,20 +7,34 @@ dirname = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(dirname, '..', '..', '..'))
 
 from SpiffWorkflow import TaskState
+from SpiffWorkflow.bpmn.PythonScriptEngine import PythonScriptEngine
 from SpiffWorkflow.bpmn.workflow import BpmnWorkflow
 from SpiffWorkflow.bpmn.exceptions import WorkflowTaskExecException
 from tests.SpiffWorkflow.bpmn.BpmnWorkflowTestCase import BpmnWorkflowTestCase
 
 __author__ = 'jbirddog'
 
+class SlackWebhookOperator(object):
+    def __init__(self, webhook_token="", message="", channel="", **kwargs):
+        super().__init(**kwargs)
+        self.channel = channel
+        self.message = message
+        self.webhook_token = webhook_token
+
+    def execute(self):
+        pass
+
 class ServiceTaskTest(BpmnWorkflowTestCase):
 
     def setUp(self):
         spec, subprocesses = self.load_workflow_spec('service_task.bpmn', 'service_task_example1')
-        self.workflow = BpmnWorkflow(spec, subprocesses)
+        additions = { 'SlackWebhookOperator': SlackWebhookOperator }
+        script_engine = PythonScriptEngine(scriptingAdditions=additions)
+        self.workflow = BpmnWorkflow(spec, subprocesses, script_engine=script_engine)
 
     def testRunThroughHappy(self):
-        pass
+        self.workflow.do_engine_steps()
+        # TODO check some indication that it ran
 
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(ServiceTaskTest)
