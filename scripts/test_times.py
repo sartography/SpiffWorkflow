6b7feb8031397840d5505f51cb2c8796e30cabf1
jbirddog <jon.herron@yahoo.com>
2022-08-21 17:02:49 -0400

    Factoring

--- scripts/test_times.py
+++ scripts/test_times.py
@@ -4,29 +4,48 @@
 import re
 import sys
 
+def regex_line_parser(pattern, handler):
+    regex = re.compile(pattern)
+    def parser(line):
+        match = regex.match(line)
+        if match:
+            return handler(match)
+        return None
+    return parser
+
+def rstripped(match):
+    return match.group(0).rstrip()
+
+def tupled(match):
+    return (match.group(1), match.group(2))
+
+
 def parse(lines):
     test_file = None
     timing = None
+    test_file_timings = []
 
-    test_file_regex = re.compile('.*?Test.py')
-    timing_regex = re.compile('Ran .* tests in .*')
+    test_file_line_parser = regex_line_parser('.*?Test.py', rstripped)
+    timing_line_parser = regex_line_parser('Ran (.*) tests in (.*)', tupled)
 
     for line in lines:
-        # line.rstrip()
         if test_file is None:
-            match = test_file_regex.match(line)
-            if match:
-                test_file = match.group(0).rstrip()
+            test_file = test_file_line_parser(line)
         elif timing is None:
-            match = timing_regex.match(line)
-            if match:
-                timing = match.group(0).rstrip()
+            timing = timing_line_parser(line)
 
         if test_file is not None and timing is not None:
-            print(f'{test_file}: {timing}')
+            test_file_timings.append((test_file, timing))
             test_file = None
             timing = None
 
+    return test_file_timings
+
+def report(parsed_data):
+    for parsed in parsed_data:
+        print(f'{parsed[0]}: {parsed[1]}')
+
 if __name__ == '__main__':
     data = sys.stdin.readlines()
-    parse(data)
+    parsed_data = parse(data)
+    report(parsed_data)
