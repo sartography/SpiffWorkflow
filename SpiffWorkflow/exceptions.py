17381944d94d6ea56f0b47df0ded4694fd8022e9
Jon Herron <jon.herron@yahoo.com>
2022-09-20 13:59:25 -0400

    PR feedback, add WorkflowTaskException to core

--- SpiffWorkflow/exceptions.py
+++ SpiffWorkflow/exceptions.py
@@ -45,5 +45,32 @@ class WorkflowException(Exception):
             task_trace.append(f"{workflow.spec.task_specs[caller].description} ({workflow.spec.file})")
         return task_trace
 
+class WorkflowTaskException(WorkflowException):
+    """WorkflowException that provides task_trace information."""
+
+    def __init__(self, task, error_msg, exception=None):
+        """
+        Exception initialization.
+
+        :param task: the task that threw the exception
+        :type task: Task
+        :param error_msg: a human readable error message
+        :type error_msg: str
+        :param exception: an exception to wrap, if any
+        :type exception: Exception
+        """
+
+        self.exception = exception
+        self.task = task
+
+        # If encountered in a sub-workflow, this traces back up the stack
+        # so we can tell how we got to this paticular task, no matter how
+        # deeply nested in sub-workflows it is.  Takes the form of:
+        # task-description (file-name)
+        self.task_trace = self.get_task_trace(task)
+
+        super().__init__(task.task_spec, error_msg)
+
+
 class StorageException(Exception):
     pass
