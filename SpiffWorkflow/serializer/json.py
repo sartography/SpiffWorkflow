fb10e1816531033160e4ea962a15338a98b0dcda
Jon Herron <jon.herron@yahoo.com>
2022-09-23 19:56:34 -0400

    Refactor for easier subclassing

--- SpiffWorkflow/serializer/json.py
+++ SpiffWorkflow/serializer/json.py
@@ -22,78 +22,72 @@ from ..operators import Attrib
 from ..bpmn.serializer.dict import BPMNDictionarySerializer
 from ..camunda.specs.UserTask import Form
 
-def object_hook(dct):
-    if '__uuid__' in dct:
-        return uuid.UUID(dct['__uuid__'])
-
-    if '__bytes__' in dct:
-        return dct['__bytes__'].encode('ascii')
-
-    if '__attrib__' in dct:
-        return Attrib(dct['__attrib__'])
-
-    if '__form__' in dct:
-        return Form(init=json.loads(dct['__form__']))
-
-
-    return dct
-
-
-def JsonableHandler(Obj):
-    if hasattr(Obj, 'jsonable'):
-        return Obj.jsonable()
-    else:
-        raise 'Object of type %s with value of %s is not JSON serializable' % (
-            type(Obj), repr(Obj))
-
-
-
-
-
-def default(obj):
-    if isinstance(obj, uuid.UUID):
-        return {'__uuid__': obj.hex}
-
-    if isinstance(obj, bytes):
-        return {'__bytes__': obj.decode('ascii')}
-
-    if isinstance(obj, Attrib):
-        return {'__attrib__': obj.name}
-
-    if isinstance(obj,Form):
-        return {'__form__': json.dumps(obj, default=JsonableHandler)}
-
-    raise TypeError('%r is not JSON serializable' % obj)
-
-
-def loads(text):
-    return json.loads(text, object_hook=object_hook)
-
-
-def dumps(dct):
-    return json.dumps(dct, sort_keys=True, default=default)
-
-
 # TODO refactor to move bpmn/camunda parts out of core
 class JSONSerializer(BPMNDictionarySerializer):
 
     def serialize_workflow_spec(self, wf_spec, **kwargs):
         thedict = super(JSONSerializer, self).serialize_workflow_spec(
             wf_spec, **kwargs)
-        return dumps(thedict)
+        return self._dumps(thedict)
 
     def deserialize_workflow_spec(self, s_state, **kwargs):
-        thedict = loads(s_state)
+        thedict = self._loads(s_state)
         return super(JSONSerializer, self).deserialize_workflow_spec(
             thedict, **kwargs)
 
     def serialize_workflow(self, workflow, **kwargs):
         thedict = super(JSONSerializer, self).serialize_workflow(
             workflow, **kwargs)
-        return dumps(thedict)
+        return self._dumps(thedict)
 
     def deserialize_workflow(self, s_state, **kwargs):
-        thedict = loads(s_state)
+        thedict = self._loads(s_state)
         return super(JSONSerializer, self).deserialize_workflow(
             thedict, **kwargs)
+
+    def _object_hook(self, dct):
+        if '__uuid__' in dct:
+            return uuid.UUID(dct['__uuid__'])
+
+        if '__bytes__' in dct:
+            return dct['__bytes__'].encode('ascii')
+
+        if '__attrib__' in dct:
+            return Attrib(dct['__attrib__'])
+
+        if '__form__' in dct:
+            return Form(init=json.loads(dct['__form__']))
+
+        return dct
+
+    def _jsonableHandler(self, obj):
+        if hasattr(obj, 'jsonable'):
+            return obj.jsonable()
+
+        raise 'Object of type %s with value of %s is not JSON serializable' % (
+            type(obj), repr(obj))
+
+
+    def _default(self, obj):
+        if isinstance(obj, uuid.UUID):
+            return {'__uuid__': obj.hex}
+
+        if isinstance(obj, bytes):
+            return {'__bytes__': obj.decode('ascii')}
+
+        if isinstance(obj, Attrib):
+            return {'__attrib__': obj.name}
+
+        if isinstance(obj,Form):
+            return {'__form__': json.dumps(obj, default=lambda o:
+                self._jsonableHandler(o))}
+
+        raise TypeError('%r is not JSON serializable' % obj)
+
+    def _loads(self, text):
+        return json.loads(text, object_hook=lambda o: self._object_hook(o))
+
+    def _dumps(self, dct):
+        return json.dumps(dct, sort_keys=True, default=lambda o:
+                self._default(o))
 
