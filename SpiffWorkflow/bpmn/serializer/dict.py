6c32c7f0cd5f77ef5f3867a857094d3989cee76c
Jon Herron <jon.herron@yahoo.com>
2022-09-23 16:38:04 -0400

    WIP

--- SpiffWorkflow/bpmn/serializer/dict.py
+++ SpiffWorkflow/bpmn/serializer/dict.py
@@ -15,9 +15,17 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301  USA
+
+from ...camunda.specs.UserTask import UserTask
+from ...dmn.engine.DMNEngine import DMNEngine
+from ...dmn.specs.BusinessRuleTask import BusinessRuleTask
+from ...dmn.specs.model import DecisionTable
 from ...serializer.dict import DictionarySerializer
+from ...util.impl import get_class
 from ..specs.ExclusiveGateway import ExclusiveGateway
+from ..specs.MultiInstanceTask import MultiInstanceTask
 from ..specs.ScriptTask import ScriptTask
+from ..specs.SubWorkflowTask import SubWorkflowTask
 
 
 class BPMNDictionarySerializer(DictionarySerializer):
@@ -80,3 +88,152 @@ class BPMNDictionarySerializer(DictionarySerializer):
         spec = ScriptTask(wf_spec, s_state['name'], s_state['script'])
         self.deserialize_task_spec(wf_spec, s_state, spec=spec)
         return spec
+
+    def serialize_subworkflow_task(self, spec):
+        s_state = self.serialize_task_spec(spec)
+        s_state['wf_class'] = spec.wf_class.__module__ + "." + spec.wf_class.__name__
+        s_state['spec'] = self.serialize_workflow_spec(spec.spec)
+        return s_state
+
+    def deserialize_subworkflow_task(self, wf_spec, s_state, cls):
+        spec = cls(wf_spec, s_state['name'])
+        spec.wf_class = get_class(s_state['wf_class'])
+        if 'spec_name' in s_state:
+            s_state['spec'] = self.SPEC_STATES[s_state['spec_name']]
+        spec.spec = self.deserialize_workflow_spec(s_state['spec'])
+        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
+        return spec
+
+    def serialize_generic_event(self, spec):
+        s_state = self.serialize_task_spec(spec)
+        if spec.event_definition:
+            s_state['event_definition'] = spec.event_definition.serialize()
+        else:
+            s_state['event_definition'] = None
+        return s_state
+
+    def deserialize_generic_event(self, wf_spec, s_state, cls):
+        if s_state.get('event_definition',None):
+            evtcls = get_class(s_state['event_definition']['classname'])
+            event = evtcls.deserialize(s_state['event_definition'])
+        else:
+            event = None
+        spec = cls(wf_spec, s_state['name'], event)
+        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
+        return spec
+
+    def serialize_boundary_event_parent(self, spec):
+        s_state = self.serialize_task_spec(spec)
+        s_state['main_child_task_spec'] = spec.main_child_task_spec.id
+        return s_state
+
+    def deserialize_boundary_event_parent(self, wf_spec, s_state, cls):
+
+        main_child_task_spec = wf_spec.get_task_spec_from_id(s_state['main_child_task_spec'])
+        spec = cls(wf_spec, s_state['name'], main_child_task_spec)
+        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
+        return spec
+
+    def serialize_boundary_event(self, spec):
+        s_state = self.serialize_task_spec(spec)
+        if spec.cancel_activity:
+            s_state['cancel_activity'] = spec.cancel_activity
+        else:
+            s_state['cancel_activity'] = None
+        if spec.event_definition:
+            s_state['event_definition'] = spec.event_definition.serialize()
+        else:
+            s_state['event_definition'] = None
+        return s_state
+
+    def deserialize_boundary_event(self, wf_spec, s_state, cls):
+        cancel_activity = s_state.get('cancel_activity',None)
+        if s_state['event_definition']:
+            eventclass = get_class(s_state['event_definition']['classname'])
+            event = eventclass.deserialize(s_state['event_definition'])
+        else:
+            event = None
+        spec = cls(wf_spec, s_state['name'], cancel_activity=cancel_activity,event_definition=event)
+        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
+        return spec
+
+    def serialize_user_task(self, spec):
+        s_state = self.serialize_task_spec(spec)
+        s_state['form'] = spec.form
+        return s_state
+
+    def deserialize_user_task(self, wf_spec, s_state):
+        spec = UserTask(wf_spec, s_state['name'], s_state['form'])
+        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
+        return spec
+
+
+    def serialize_business_rule_task(self, spec):
+        s_state = self.serialize_task_spec(spec)
+        dictrep = spec.dmnEngine.decisionTable.serialize()
+        # future
+        s_state['dmn'] = dictrep
+        return s_state
+
+    def deserialize_business_rule_task(self, wf_spec, s_state):
+        dt = DecisionTable(None,None)
+        dt.deserialize(s_state['dmn'])
+        dmnEngine = DMNEngine(dt)
+        spec = BusinessRuleTask(wf_spec, s_state['name'], dmnEngine)
+        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
+        return spec
+
+    def serialize_multi_instance(self, spec):
+        s_state = super().serialize_multi_instance(spec)
+        # here we need to add in all of the things that would get serialized
+        # for other classes that the MultiInstance could be -
+        #
+        if hasattr(spec,'form'):
+            s_state['form'] = spec.form
+
+        if isinstance(spec,MultiInstanceTask):
+            s_state['collection'] = self.serialize_arg(spec.collection)
+            s_state['elementVar'] = self.serialize_arg(spec.elementVar)
+            s_state['completioncondition'] = self.serialize_arg(spec.completioncondition)
+            s_state['isSequential'] = self.serialize_arg(spec.isSequential)
+            s_state['loopTask'] = self.serialize_arg(spec.loopTask)
+            if (hasattr(spec,'expanded')):
+                s_state['expanded'] = self.serialize_arg(spec.expanded)
+        if isinstance(spec,BusinessRuleTask):
+            brState = self.serialize_business_rule_task(spec)
+            s_state['dmn'] = brState['dmn']
+        if isinstance(spec, ScriptTask):
+            brState = self.serialize_script_task(spec)
+            s_state['script'] = brState['script']
+        if isinstance(spec, SubWorkflowTask):
+            brState = self.serialize_subworkflow(spec)
+            s_state['wf_class'] = brState['wf_class']
+            s_state['spec'] = brState['spec']
+
+        return s_state
+
+    def deserialize_multi_instance(self, wf_spec, s_state, cls=None):
+        cls = super().deserialize_multi_instance(wf_spec, s_state, cls)
+        if isinstance(cls,MultiInstanceTask):
+            cls.isSequential = self.deserialize_arg(s_state['isSequential'])
+            cls.loopTask = self.deserialize_arg(s_state['loopTask'])
+            cls.elementVar = self.deserialize_arg(s_state['elementVar'])
+            cls.completioncondition = self.deserialize_arg(s_state['completioncondition'])
+            cls.collection = self.deserialize_arg(s_state['collection'])
+            if s_state.get('expanded',None):
+                cls.expanded = self.deserialize_arg(s_state['expanded'])
+        if isinstance(cls,BusinessRuleTask):
+            dt = DecisionTable(None,None)
+            dt.deserialize(s_state['dmn'])
+            dmnEngine = DMNEngine(dt)
+            cls.dmnEngine=dmnEngine
+        if isinstance(cls, ScriptTask):
+            cls.script = s_state['script']
+        if isinstance(cls, SubWorkflowTask):
+            cls.wf_class = get_class(s_state['wf_class'])
+            cls.spec = self.deserialize_workflow_spec(s_state['spec'])
+
+        if s_state.get('form',None):
+            cls.form = s_state['form']
+
+        return cls
--- SpiffWorkflow/serializer/dict.py
+++ SpiffWorkflow/serializer/dict.py
@@ -304,100 +304,6 @@ class DictionarySerializer(Serializer):
         self.deserialize_task_spec(wf_spec, s_state, spec=spec)
         return spec
 
-    def serialize_subworkflow_task(self, spec):
-        s_state = self.serialize_task_spec(spec)
-        s_state['wf_class'] = spec.wf_class.__module__ + "." + spec.wf_class.__name__
-        s_state['spec'] = self.serialize_workflow_spec(spec.spec)
-        return s_state
-
-    def deserialize_subworkflow_task(self, wf_spec, s_state, cls):
-        spec = cls(wf_spec, s_state['name'])
-        spec.wf_class = get_class(s_state['wf_class'])
-        if 'spec_name' in s_state:
-            s_state['spec'] = self.SPEC_STATES[s_state['spec_name']]
-        spec.spec = self.deserialize_workflow_spec(s_state['spec'])
-        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
-        return spec
-
-    def serialize_generic_event(self, spec):
-        s_state = self.serialize_task_spec(spec)
-        if spec.event_definition:
-            s_state['event_definition'] = spec.event_definition.serialize()
-        else:
-            s_state['event_definition'] = None
-        return s_state
-
-    def deserialize_generic_event(self, wf_spec, s_state, cls):
-        if s_state.get('event_definition',None):
-            evtcls = get_class(s_state['event_definition']['classname'])
-            event = evtcls.deserialize(s_state['event_definition'])
-        else:
-            event = None
-        spec = cls(wf_spec, s_state['name'], event)
-        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
-        return spec
-
-    def serialize_boundary_event_parent(self, spec):
-        s_state = self.serialize_task_spec(spec)
-        s_state['main_child_task_spec'] = spec.main_child_task_spec.id
-        return s_state
-
-    def deserialize_boundary_event_parent(self, wf_spec, s_state, cls):
-
-        main_child_task_spec = wf_spec.get_task_spec_from_id(s_state['main_child_task_spec'])
-        spec = cls(wf_spec, s_state['name'], main_child_task_spec)
-        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
-        return spec
-
-    def serialize_boundary_event(self, spec):
-        s_state = self.serialize_task_spec(spec)
-        if spec.cancel_activity:
-            s_state['cancel_activity'] = spec.cancel_activity
-        else:
-            s_state['cancel_activity'] = None
-        if spec.event_definition:
-            s_state['event_definition'] = spec.event_definition.serialize()
-        else:
-            s_state['event_definition'] = None
-        return s_state
-
-    def deserialize_boundary_event(self, wf_spec, s_state, cls):
-        cancel_activity = s_state.get('cancel_activity',None)
-        if s_state['event_definition']:
-            eventclass = get_class(s_state['event_definition']['classname'])
-            event = eventclass.deserialize(s_state['event_definition'])
-        else:
-            event = None
-        spec = cls(wf_spec, s_state['name'], cancel_activity=cancel_activity,event_definition=event)
-        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
-        return spec
-
-    def serialize_user_task(self, spec):
-        s_state = self.serialize_task_spec(spec)
-        s_state['form'] = spec.form
-        return s_state
-
-    def deserialize_user_task(self, wf_spec, s_state):
-        spec = UserTask(wf_spec, s_state['name'], s_state['form'])
-        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
-        return spec
-
-
-    def serialize_business_rule_task(self, spec):
-        s_state = self.serialize_task_spec(spec)
-        dictrep = spec.dmnEngine.decisionTable.serialize()
-        # future
-        s_state['dmn'] = dictrep
-        return s_state
-
-    def deserialize_business_rule_task(self, wf_spec, s_state):
-        dt = DecisionTable(None,None)
-        dt.deserialize(s_state['dmn'])
-        dmnEngine = DMNEngine(dt)
-        spec = BusinessRuleTask(wf_spec, s_state['name'], dmnEngine)
-        self.deserialize_task_spec(wf_spec, s_state, spec=spec)
-        return spec
-
     def serialize_join(self, spec):
         s_state = self.serialize_task_spec(spec)
         s_state['split_task'] = spec.split_task
@@ -449,33 +355,12 @@ class DictionarySerializer(Serializer):
         # here we need to add in all of the things that would get serialized
         # for other classes that the MultiInstance could be -
         #
-        if hasattr(spec,'form'):
-            s_state['form'] = spec.form
 
-        if isinstance(spec,MultiInstanceTask):
-            s_state['collection'] = self.serialize_arg(spec.collection)
-            s_state['elementVar'] = self.serialize_arg(spec.elementVar)
-            s_state['completioncondition'] = self.serialize_arg(spec.completioncondition)
-            s_state['isSequential'] = self.serialize_arg(spec.isSequential)
-            s_state['loopTask'] = self.serialize_arg(spec.loopTask)
-            if (hasattr(spec,'expanded')):
-                s_state['expanded'] = self.serialize_arg(spec.expanded)
-        if isinstance(spec,BusinessRuleTask):
-            brState = self.serialize_business_rule_task(spec)
-            s_state['dmn'] = brState['dmn']
-        if isinstance(spec, ScriptTask):
-            brState = self.serialize_script_task(spec)
-            s_state['script'] = brState['script']
         if isinstance(spec, SubWorkflow):
             brState = self.serialize_sub_workflow(spec)
             s_state['file'] = brState['file']
             s_state['in_assign'] = brState['in_assign']
             s_state['out_assign'] = brState['out_assign']
-        if isinstance(spec, SubWorkflowTask):
-            brState = self.serialize_subworkflow(spec)
-            s_state['wf_class'] = brState['wf_class']
-            s_state['spec'] = brState['spec']
-
 
         s_state['times'] = self.serialize_arg(spec.times)
         s_state['prevtaskclass'] = spec.prevtaskclass
@@ -489,21 +374,6 @@ class DictionarySerializer(Serializer):
         if isinstance(s_state['times'],list):
             s_state['times'] = self.deserialize_arg(s_state['times'])
             cls.times = s_state['times']
-        if isinstance(cls,MultiInstanceTask):
-            cls.isSequential = self.deserialize_arg(s_state['isSequential'])
-            cls.loopTask = self.deserialize_arg(s_state['loopTask'])
-            cls.elementVar = self.deserialize_arg(s_state['elementVar'])
-            cls.completioncondition = self.deserialize_arg(s_state['completioncondition'])
-            cls.collection = self.deserialize_arg(s_state['collection'])
-            if s_state.get('expanded',None):
-                cls.expanded = self.deserialize_arg(s_state['expanded'])
-        if isinstance(cls,BusinessRuleTask):
-            dt = DecisionTable(None,None)
-            dt.deserialize(s_state['dmn'])
-            dmnEngine = DMNEngine(dt)
-            cls.dmnEngine=dmnEngine
-        if isinstance(cls, ScriptTask):
-            cls.script = s_state['script']
         if isinstance(cls, SubWorkflow):
             if s_state.get('file'):
                 cls.file = self.deserialize_arg(s_state['file'])
@@ -511,15 +381,11 @@ class DictionarySerializer(Serializer):
                 cls.file = None
             cls.in_assign = self.deserialize_list(s_state['in_assign'])
             cls.out_assign = self.deserialize_list(s_state['out_assign'])
-        if isinstance(cls, SubWorkflowTask):
-            cls.wf_class = get_class(s_state['wf_class'])
-            cls.spec = self.deserialize_workflow_spec(s_state['spec'])
-
-        if s_state.get('form',None):
-            cls.form = s_state['form']
 
         self.deserialize_task_spec(wf_spec, s_state, spec=cls)
         return cls
+
+# TODO - got this far
 
     def serialize_release_mutex(self, spec):
         s_state = self.serialize_task_spec(spec)
