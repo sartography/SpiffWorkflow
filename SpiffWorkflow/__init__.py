14d657765e8ae2c8434e7d18ffd51a964c8f5fee
Jon Herron <jon.herron@yahoo.com>
2022-09-23 21:20:05 -0400

    Remove bpmn references from __init__.py

--- SpiffWorkflow/__init__.py
+++ SpiffWorkflow/__init__.py
@@ -5,11 +5,6 @@ from .workflow import Workflow
 from .task import Task, TaskState, TaskStateNames
 from .exceptions import WorkflowException
 from .navigation import NavItem
-from .bpmn.specs.BpmnSpecMixin import BpmnSpecMixin, SequenceFlow
-from .bpmn.specs.UnstructuredJoin import UnstructuredJoin
-from .bpmn.specs.MultiInstanceTask import MultiInstanceTask
-from .bpmn.specs.SubWorkflowTask import CallActivity, TransactionSubprocess
-from .bpmn.specs.events import _BoundaryEventParent
 
 import inspect
 
