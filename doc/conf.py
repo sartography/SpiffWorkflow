a094adad8767f82e9c5fa806a46597e066252a72
Dan <daniel.h.funk@gmail.com>
2022-10-19 10:16:28 -0400

    Prep for release 1.2.1 - Minor fix for a dependency in Python 3.10

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Sartography'
 author = 'Sartography'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.0'
+release = '1.2.1'
 
 # -- General configuration ---------------------------------------------------
 
