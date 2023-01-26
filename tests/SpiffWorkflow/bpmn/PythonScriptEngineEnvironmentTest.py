b11829dfa9aea7d016d41e3ecea6347ea255d334
Jon Herron <jon.herron@yahoo.com>
2023-01-26 13:31:57 -0500

    Set up the failing test

--- tests/SpiffWorkflow/bpmn/PythonScriptEngineEnvironmentTest.py
+++ tests/SpiffWorkflow/bpmn/PythonScriptEngineEnvironmentTest.py
@@ -13,8 +13,22 @@ class PythonScriptEngineEnvironmentTest(BpmnWorkflowTestCase):
     def testTaskDataSizeWithDefaultPythonScriptEngine(self):
         self.workflow.do_engine_steps()
         task_data_len = self._get_task_data_len()
+        d_uniques = set(self.workflow.data["d"])
+        d_len = len(self.workflow.data["d"])
 
         self.assertGreater(task_data_len, 1024)
+        self.assertEqual(d_len, 512*3)
+        self.assertEqual(d_uniques, {"a", "b", "c"})
+
+    def testTaskDataSizeWithEnvironmentBasedPythonScriptEngine(self):
+        self.workflow.do_engine_steps()
+        task_data_len = self._get_task_data_len()
+        d_uniques = set(self.workflow.data["d"])
+        d_len = len(self.workflow.data["d"])
+
+        self.assertLess(task_data_len, 1024)
+        self.assertEqual(d_len, 512*3)
+        self.assertEqual(d_uniques, {"a", "b", "c"})
 
     def _get_task_data_len(self):
         tasks_to_check = self.workflow.get_tasks(TaskState.FINISHED_MASK)
