ea5ffff417e32e64cc5c8d5dcd831d89d17cbd79
Elizabeth Esswein <elizabeth.esswein@gmail.com>
2023-03-17 19:28:44 -0400

    create tests based on individual patterns

---
+++ tests/SpiffWorkflow/core/ControlFlowPatternTest.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+from .pattern_base import WorkflowPatternTestCase
+
+# This combines the old pattern tests with the old serializer tests, creating one test per pattern
+# that tests the tasks in it can be serialized with our serializers and the workflows run with the
+# expected output.  This format is a little annoying (inheriting from two classes with the actual 
+# work being done in the secondary class); however, this is the most concise thing I could manage.
+#
+# There were also a fair amount of never-used options in those tests, so the tests in the base case
+# are a lot simpler than the ones they replaced.
+
+class SequenceTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/sequence')
+
+class ParallelSplitTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/parallel_split')
+
+class SynchronizationTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/synchronization')
+
+class ExclusiveChoiceTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/exclusive_choice')
+
+class SimpleMergeTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/simple_merge')
+
+class MultiChoiceTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/multi_choice')
+
+class StructuredSynchronizingMergeTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/structured_synchronizing_merge')
+
+class MultiMergeTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/multi_merge')
+
+class StructuredDiscriminatorTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/structured_discriminator')
+
+class BlockingDiscriminatorTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/blocking_discriminator')
+
+class CacncellingDiscriminatorTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/cancelling_discriminator')
+
+class StructuredPartialJoin(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/structured_partial_join')
+
+class BlockingPartialJoin(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/blocking_partial_join')
+
+class CancellingPartialJoin(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/cancelling_partial_join')
+
+class GeneralizedAndJoin(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/generalized_and_join')
+
+class LocalSynchronizingMergeTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/acyclic_synchronizing_merge')
+
+class GeneralSynchronizingMergeTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/general_synchronizing_merge')
+
+class ThreadMergeTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/thread_merge')
+
+class ThreadSplitTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/thread_split')
+
+class MultiInstanceWithoutSynchonizationTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/multi_instance_without_synch')
+
+class MultiInstanceWithDesignTimeKnowledgeTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/multi_instance_with_a_priori_design_time_knowledge')
+
+class MultiInstanceWithRunTimeKnowledgeTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/multi_instance_with_a_priori_run_time_knowledge')
+
+class StaticPartialJoinMultiInstanceTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/static_partial_join_for_multi_instance')
+
+class CancellingPartialJoinMultiInstanceTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/cancelling_partial_join_for_multi_instance')
+
+class DynamicPartialJoinMultiInstanceTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/dynamic_partial_join_for_multi_instance')
+
+class DeferredChoiceTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/deferred_choice')
+
+class InterleavedParallelRoutingTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/interleaved_parallel_routing')
+
+class MilestoneTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/milestone')
+
+class CriticalSectionTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/critical_section')
+
+class InterleavedRoutingTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/interleaved_routing')
+
+class CancelTaskTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/cancel_task')
+
+class CancelCaseTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/cancel_case')
+
+class CancelRegionTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/cancel_region')
+
+class CancelMultiInstanceTaskTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/cancel_multi_instance_task')
+
+class CompleteMultiInstanceTaskTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/complete_multiple_instance_activity')
+
+class ArbitraryCyclesTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/arbitrary_cycles')
+
+class RecursionTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/recursion')
+
+class ImplicitTerminationTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/implicit_termination')
+
+class ExplicitTerminationTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/explicit_termination')
+
+class TransientTriggerTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/transient_trigger')
+
+class PersistentTriggerTest(TestCase, WorkflowPatternTestCase):
+    def setUp(self):
+        self.load_from_xml('control-flow/persistent_trigger')
---
+++ tests/SpiffWorkflow/core/pattern_base.py
@@ -0,0 +1,123 @@
+import os
+import time
+import warnings
+
+from lxml import etree
+
+from SpiffWorkflow.workflow import Workflow
+from SpiffWorkflow.task import Task
+from SpiffWorkflow.specs.WorkflowSpec import WorkflowSpec
+
+from SpiffWorkflow.serializer.prettyxml import XmlSerializer as PrettyXmlSerializer
+from SpiffWorkflow.serializer.xml import XmlSerializer
+from SpiffWorkflow.serializer.dict import DictionarySerializer
+from SpiffWorkflow.serializer.json import JSONSerializer
+
+from SpiffWorkflow.serializer.exceptions import TaskNotSupportedError
+
+from .util import track_workflow
+
+data_dir = os.path.join(os.path.dirname(__file__), 'data')
+xml_serializer = XmlSerializer()
+dict_serializer = DictionarySerializer()
+json_serializer = JSONSerializer()
+
+class WorkflowPatternTestCase:
+
+    def init_thread_pool(self):
+        Task.id_pool = 0
+        Task.thread_id_pool = 0
+
+    def load_from_xml(self, pattern):
+
+        self.init_thread_pool()
+        prefix = os.path.join(data_dir, pattern)
+        filename = f'{prefix}.xml'
+        with open(filename) as fp:
+            xml = etree.parse(fp).getroot()
+        # This "serializer" is a parser; it doesn't deserialize.
+        # Because we use it to load all the workflows, consider it tested here.
+        serializer = PrettyXmlSerializer()
+        self.spec = WorkflowSpec.deserialize(serializer, xml, filename=filename)
+
+        path_file = f'{prefix}.path'
+        if os.path.exists(path_file):
+            with open(path_file) as fp:
+                self.expected_path = fp.read()
+        else:
+            self.expected_path = None
+
+        data_file = f'{prefix}.data'
+        if os.path.exists(data_file):
+            with open(data_file) as fp:
+                self.expected_data = fp.read()
+        else:
+            self.expected_data = None
+
+        self.taken_path = track_workflow(self.spec)
+        self.workflow = Workflow(self.spec)
+
+    def serialize(self, spec_or_workflow, serializer):
+
+        try:
+            before = spec_or_workflow.serialize(serializer)
+            restored = spec_or_workflow.deserialize(serializer, before)
+            after = restored.serialize(serializer)
+            return before, after
+        except TaskNotSupportedError as exc:
+            warnings.warn(f'Unsupported task spec: {exc}')
+            return None, None
+
+    def run_workflow(self):
+        # We allow the workflow to require a maximum of 5 seconds to complete, to allow for testing long running tasks.
+        for i in range(10):
+            self.workflow.complete_all(False)
+            if self.workflow.is_completed():
+                break
+            time.sleep(0.5)
+
+    def test_run_workflow(self):
+
+        self.run_workflow()
+        self.assertTrue(self.workflow.is_completed())
+
+        # Check whether the correct route was taken.
+        if self.expected_path is not None:
+            taken_path = '\n'.join(self.taken_path) + '\n'
+            self.assertEqual(taken_path, self.expected_path)
+
+        # Check data availibility.
+        if self.expected_data is not None:
+            result = self.workflow.get_data('data', '')
+            self.assertIn(result, self.expected_data)
+
+    def test_xml_serializer(self):
+
+        prepare_result = lambda item: etree.tostring(item, pretty_print=True)
+
+        before, after = self.serialize(self.spec, xml_serializer)
+        self.assertEqual(prepare_result(before), prepare_result(after))
+        self.assertIsInstance(before, etree._Element)
+
+        before, after = self.serialize(self.workflow, xml_serializer)
+        if before is not None:
+            self.assertEqual(prepare_result(before), prepare_result(after))
+
+    def test_dictionary_serializer(self):
+
+        before, after = self.serialize(self.spec, dict_serializer)
+        self.assertDictEqual(before, after)
+        self.assertIsInstance(before, dict)
+
+        before, after = self.serialize(self.workflow, dict_serializer)
+        if before is not None:
+            self.assertDictEqual(before, after)
+
+    def test_json_serializer(self):
+
+        before, after = self.serialize(self.spec, json_serializer)
+        self.assertEqual(before, after)
+        self.assertIsInstance(before, str)
+
+        before, after = self.serialize(self.workflow, json_serializer)
+        self.assertEqual(before, after)
