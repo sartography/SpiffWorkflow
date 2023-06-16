a96fc78e3a8b57ec6568c7129c9cea5e05eccc7e
danfunk <daniel.h.funk@gmail.com>
2023-06-16 15:35:03 -0400

    fixing docs

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Sartography'
 author = 'Sartography'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.1'
 
 # -- General configuration ---------------------------------------------------
 
