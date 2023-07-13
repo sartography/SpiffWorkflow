a7d81c02dd084492e235b4b51082a31d9ec5549c
Elizabeth Esswein <elizabeth.esswein@gmail.com>
2023-07-13 18:35:21 -0400

    prevent parallel gateways from waiting on descendants

---
+++ tests/SpiffWorkflow/bpmn/parallel_gateway_tests/ParallelGatewayLoopInputTest.py
@@ -0,0 +1,19 @@
+from SpiffWorkflow.task import TaskState
+from SpiffWorkflow.bpmn.workflow import BpmnWorkflow
+from ..BpmnWorkflowTestCase import BpmnWorkflowTestCase
+
+class ParallelGatewayLoopInputTest(BpmnWorkflowTestCase):
+
+    def setUp(self):
+        spec, subprocess_specs = self.load_workflow_spec('gateway_loop_input.bpmn', 'main')
+        self.workflow = BpmnWorkflow(spec, subprocess_specs)
+    
+    def test_loop_input(self):
+
+        self.workflow.do_engine_steps()
+        ready = self.workflow.get_tasks(TaskState.READY)
+        self.assertEqual(len(ready), 1)
+        ready[0].run()
+        self.workflow.do_engine_steps()
+        self.assertTrue(self.workflow.is_completed())
+        self.assertDictEqual(self.workflow.data, { 'x': 2})
