e6bc37764d176f8c29c8eb57d5d9071088694f9c
Elizabeth Esswein <elizabeth.esswein@gmail.com>
2024-11-26 22:29:48 -0500

    remove feel engine

--- tests/SpiffWorkflow/dmn/feel_engine/FeelDecisionRunner.py
+++
@@ -1,8 +0,0 @@
-from SpiffWorkflow.bpmn.script_engine.feel_engine import FeelLikeScriptEngine
-
-from ..DecisionRunner import DecisionRunner
-
-class FeelDecisionRunner(DecisionRunner):
-
-    def __init__(self, filename):
-        super().__init__(FeelLikeScriptEngine(), filename, 'feel_engine')
