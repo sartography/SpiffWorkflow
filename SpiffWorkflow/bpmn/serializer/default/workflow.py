28756c05d1c5b937fe0bd5ffc221ed9f0e237172
Elizabeth Esswein <elizabeth.esswein@gmail.com>
2026-06-07 10:03:00 -0400

    revert timer handling

--- SpiffWorkflow/bpmn/serializer/default/workflow.py
+++ SpiffWorkflow/bpmn/serializer/default/workflow.py
@@ -201,7 +201,6 @@ class BpmnWorkflowConverter(WorkflowConverter):
         # Handle the remaining top workflow attributes
         self.subprocesses_from_dict(dct['subprocesses'], workflow)
         workflow.bpmn_events = self.registry.restore(dct.pop('bpmn_events', []))
-        workflow._rebuild_waiting_task_index()
 
         return workflow
 
